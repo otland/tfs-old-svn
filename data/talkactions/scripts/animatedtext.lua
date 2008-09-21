f5eff483c2020e88b9c5fed50961086452c0d10b
Dawid Pietrzak <fightingelf@gmail.com>
2008-09-21 00:51:59 +0000

    * Fixed !y test talkaction * Updated docs (perhaps, someone could take a look on it)
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@68 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- data/talkactions/scripts/animatedtext.lua
+++ data/talkactions/scripts/animatedtext.lua
@@ -2,12 +2,15 @@ function onSay(cid, words, param)
 	if param ~= nil then
 		local seperator = string.find(param, ", ")
 		if seperator ~= nil then
-			local text = string.sub(param, 1, seperator -1 )
-			local color = string.sub(value, seperator + 2, string.len(value))
-
-			doSendAnimatedText(getCreaturePosition(cid), text, color)
+			local text = string.sub(param, 1, seperator - 1)
+			local color = string.sub(param, seperator + 2, string.len(param))
+			if(color > 0 and color < 255)
+				doSendAnimatedText(getCreaturePosition(cid), text, color)
+			else
+				doPlayerSendCancel(cid, "Typed color has to be between 0 and 255")
+			end
 		else
-			doPlayerSendCancel(cid, "You need to type an effect.")
+			doPlayerSendCancel(cid, "You need to type color.")
 		end
 	else
 		doPlayerSendCancel(cid, "You need to type text.")
