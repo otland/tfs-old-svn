baba68595369c25157d287ed57998090a4d91b86
Mark Samman <mark.samman@gmail.com>
2008-10-08 09:51:38 +0000

    * Fix in getPlayerByName.
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@217 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- data/compat.lua
+++ data/compat.lua
@@ -72,7 +72,7 @@ end
 
 function getPlayerByName(name)
 	local cid = getCreatureByName(name)
-	return isPlayer(creature) and cid or nil
+	return isPlayer(creature) == TRUE and cid or nil
 end
 
 function hasCondition(cid, condition)
