b2f29f2c2350cf6563711e88198219b98fb3d7e5
Dawid Pietrzak <fightingelf@gmail.com>
2008-10-18 21:29:47 +0000

    * formating
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@263 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- data/npc/lib/npcsystem/modules.lua
+++ data/npc/lib/npcsystem/modules.lua
@@ -65,7 +65,7 @@ if(Modules == nil) then
 	--Usage:
 		-- local node1 = keywordHandler:addKeyword({'promot'}, StdModule.say, {npcHandler = npcHandler, text = 'I can promote you for 20000 gold coins. Do you want me to promote you?'})
 		-- 		node1:addChildKeyword({'yes'}, StdModule.promotePlayer, {npcHandler = npcHandler, cost = 20000, promotion = 1, level = 20}, text = 'Congratulations! You are now promoted.')
-		-- 		node1:addChildKeyword({'no'}, StdModule.say, {npcHandler = npcHandler, text = 'Allright then. Come back when you are ready.'}, reset = true)
+		-- 		node1:addChildKeyword({'no'}, StdModule.say, {npcHandler = npcHandler, text = 'Alright then, come back when you are ready.'}, reset = true)
 	function StdModule.promotePlayer(cid, message, keywords, parameters, node)
 		local npcHandler = parameters.npcHandler
 		if(npcHandler == nil) then
