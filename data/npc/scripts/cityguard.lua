a2c6950a8dcb642b1b8d3e704d200aa486d9cb93
Dawid Pietrzak <fightingelf@gmail.com>
2008-10-19 19:29:38 +0000

    * last fix to cityguard npc
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@289 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- data/npc/scripts/cityguard.lua
+++ data/npc/scripts/cityguard.lua
@@ -1,15 +1,12 @@
-
 local target = 0
 local prevTarget = 0
 local maxChaseDistance = 20
 local origPos = 0
-local origDir = NORTH
 local lastAttack = 0
 local followTimeout = 10
 
 local function isSkulled(cid)
-	local skullType = getPlayerSkullType(cid)
-	if(skullType >= 3) then
+	if(getPlayerSkullType(cid) >= 3 and isPlayerPzLocked(cid) == TRUE) then
 		return true
 	end
 
@@ -27,24 +24,26 @@ local function updateTarget()
 	if(isPlayer(target) == FALSE) then
 		goToOrigPos()
 	elseif(not isSkulled(target)) then
-		target = 0
 		selfSay("Now, behave in the future.")
+		goToOrigPos()
 	end
 
 	if(target == 0) then
-		local list = getSpectators(getNpcPos(), 8, 8, false)
-		for i=1, table.getn(list) do
+		local list = getSpectators(getNpcPos(), 9, 9, false)
+		for i = 1, table.getn(list) do
 			local _target = list[i]
 			if(_target ~= 0) then
 				if(isPlayer(_target) == TRUE and isSkulled(_target)) then
-					if(selfFollow(_target)) then
-						target = _target
-						if(target ~= prevTarget) then
-							selfSay("We do not tolerate people like you here!")
-						end
+					if(getTilePzInfo(getCreaturePosition(_target)) == FALSE) then
+						if(selfFollow(_target)) then
+							target = _target
+							if(target ~= prevTarget) then
+								selfSay("We do not tolerate people like you here!")
+							end
 
-						prevTarget = target
-						break
+							prevTarget = target
+							break
+						end
 					end
 				end
 			end
@@ -54,14 +53,12 @@ end
 
 function onCreatureAppear(cid)
 	if(cid == getNpcCid()) then
-		--Wake up call
 		origPos = getNpcPos()
-		--origLook = getCreatureDir(cid)
 	end
 end
 
 function onCreatureDisappear(cid)
-	if(target == cid) then
+	if(cid == target) then
 		goToOrigPos()
 	end
 end
@@ -96,15 +93,13 @@ function onThink()
 	end
 
 	if(os.clock() - lastAttack > followTimeout) then
-		--To prevent bugging the npc by going to a place where he can't reach
 		selfSay("You got me this time, but just wait.")
 		goToOrigPos()
 		return
 	end
 
-	if( (math.abs(playerPos.x - myPos.x) <= 1) and (math.abs(playerPos.y - myPos.y) <= 1)) then
+	if((math.abs(playerPos.x - myPos.x) <= 1) and (math.abs(playerPos.y - myPos.y) <= 1)) then
 		doTargetCombatHealth(getNpcCid(), target, COMBAT_LIFEDRAIN, -200, -300, CONST_ME_BLOCKHIT)
 		lastAttack = os.clock()
 	end
-
 end
