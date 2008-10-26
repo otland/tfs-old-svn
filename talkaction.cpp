c6f76811ce9957c96fce64062d46e48a94aac53f
Dawid Pietrzak <fightingelf@gmail.com>
2008-10-26 03:40:03 +0000

    ok, I fail, in epic style.
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@352 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- talkaction.cpp
+++ talkaction.cpp
@@ -90,7 +90,7 @@ TalkResult_t TalkActions::onPlayerSay(Player* player, uint16_t channelId, const 
 	{
 		cmdstring[TALKFILTER_QUOTATION] = std::string(words, 0, loc);
 		paramstring[TALKFILTER_QUOTATION] = std::string(words, (loc + 1), (words.size() - loc - 1));
-		trimString(cmdstring[TALKFILTER_QUOTATION], " ");
+		trimString(cmdstring[TALKFILTER_QUOTATION]);
 	}
 
 	loc = words.find(" ", 0);
