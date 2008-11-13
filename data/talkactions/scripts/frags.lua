03a1fdc38a6c187c013abaebceb0004537eb44b3
Dawid Pietrzak <fightingelf@gmail.com>
2008-11-13 12:30:24 +0000

    * Fixed frags command
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@480 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- data/talkactions/scripts/frags.lua
+++ data/talkactions/scripts/frags.lua
@@ -5,12 +5,11 @@ local config = {
 function onSay(cid, words, param)
 	local amount = getPlayerRedSkullTicks(cid)
 	if(amount > 0 and config.fragTime > 0) then
-		local frags = (amount / config.fragTime) + 1;
-		local remainingTime = amount - (config.fragTime * (frags - 1));
-		frags = math.ceil(frags)
+		local frags = math.floor((amount / config.fragTime) + 1)
+		local remainingTime = math.floor(amount - (config.fragTime * (frags - 1)))
 
-		local hours = math.floor((remainingTime / 1000) / 60) / 60;
-		local minutes = math.floor((remainingTime / 1000) / 60) - (hours * 60);
+		local hours = math.floor(((remainingTime / 1000) / 60) / 60)
+		local minutes = math.floor(((remainingTime / 1000) / 60) - (hours * 60))
 		doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "You have " .. frags .. " unjustified frag" .. (frags > 2 and "s" or "") .. ". The amount of unjustified frags will decrease after: " .. hours .. "h and " .. minutes .. "m.")
 	else
 		doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "You do not have any unjustified frag.")	
