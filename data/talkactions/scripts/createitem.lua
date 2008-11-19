f97269caa3a9081553082d01438655caddc62cbf
Dawid Pietrzak <fightingelf@gmail.com>
2008-11-19 18:43:33 +0000

    * Added possibility to create items on tile and on look tile with /i
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@555 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- data/talkactions/scripts/createitem.lua
+++ data/talkactions/scripts/createitem.lua
@@ -5,9 +5,16 @@ function onSay(cid, words, param)
 	end
 
 	local t = string.explode(param, ",")
+	local ret = RET_NO_ERROR
+	local tmp = getCreaturePosition(cid)
+
 	local id = tonumber(t[1])
 	if(not id) then
 		id = getItemIdByName(t[1])
+		if(id == LUA_ERROR) then
+			doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "Item wich such name does not exists.")
+			return TRUE
+		end
 	end
 
 	local amount = 100
@@ -15,12 +22,24 @@ function onSay(cid, words, param)
 		amount = t[2]
 	end
 
-	local ret = doPlayerAddItem(cid, id, amount, 1)
-	if(not ret or ret == LUA_ERROR) then
+	local item = doCreateItemEx(id, amount)
+	if(t[3]) then
+		if(getBooleanFromString(t[3]) == TRUE)
+			if(t[4] and getBooleanFromString(t[4]) == TRUE) then
+				tmp = getPlayerLookPos(cid)
+			end
+
+			ret = doTileAddItemEx(tmp, item)
+		else
+			ret = doPlayerAddItemEx(cid, item, TRUE)
+		end
+	end
+
+	if(ret ~= LUA_NO_ERROR) then
 		doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "Couldn't add item: " .. t[1])
 		return TRUE
 	end
 
-	doSendMagicEffect(getCreaturePosition(cid), CONST_ME_MAGIC_RED)
+	doSendMagicEffect(tmp, CONST_ME_MAGIC_RED)
 	return TRUE
 end
