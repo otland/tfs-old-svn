076528ca852b8983fdbd3d2f1330a51357fb996e
Dawid Pietrzak <fightingelf@gmail.com>
2008-11-21 17:46:03 +0000

    * Fixed changender.lua
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@582 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- data/talkactions/scripts/changender.lua
+++ data/talkactions/scripts/changender.lua
@@ -1,18 +1,25 @@
 function onSay(cid, words, param)
-	if getPlayerPremiumDays(cid) > 2 then
-		if getPlayerPremiumDays(cid) < 65535 then
+	if(getPlayerSex(cid) == 2) then
+		doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "You cannot change your gender.")
+		return TRUE
+	end
+
+	if(getPlayerPremiumDays(cid) > 2) then
+		if(getPlayerPremiumDays(cid) < 65535) then
 			doPlayerAddPremiumDays(cid, -3)
 		end
 
-		if getPlayerSex(cid) == 0 then
+		if(getPlayerSex(cid) == 0) then
 			doPlayerSetSex(cid, 1)
 		else
 			doPlayerSetSex(cid, 0)
 		end
-		doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "You have changed your sex and lost three days of premium account.")
+
+		doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "You have changed your gender and lost three days of premium time.")
+		doSendMagicEffect(getCreaturePosition(cid), CONST_ME_MAGIC_RED)
 	else
-		doPlayerSendCancel(cid, "You do not have enough premium days, changing sex costs three of your premium days.")
-		doSendMagicEffect(getPlayerPosition(cid), CONST_ME_POFF)
+		doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "Sorry, not enough premium time- changing gender costs three days.")
+		doSendMagicEffect(getCreaturePosition(cid), CONST_ME_POFF)
 	end
 	return TRUE
 end
