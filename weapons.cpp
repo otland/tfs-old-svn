5e926e5bc6f374c9fc2dc62db628a7e331d88246
Dawid Pietrzak <fightingelf@gmail.com>
2008-11-22 00:13:42 +0000

    * Updated as in 0.2 ;)
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@592 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- weapons.cpp
+++ weapons.cpp
@@ -155,13 +155,13 @@ bool Weapons::registerEvent(Event* event, xmlNodePtr p)
 
 int32_t Weapons::getMaxMeleeDamage(int32_t attackSkill, int32_t attackValue)
 {
-	return ((int32_t)std::ceil((attackSkill * (attackValue * 0.05)) + (attackValue * 0.5)));
+	return (int32_t)std::ceil((attackSkill * (attackValue * 0.05)) + (attackValue * 0.5));
 }
 
 int32_t Weapons::getMaxWeaponDamage(int32_t level, int32_t attackSkill, int32_t attackValue, float attackFactor)
 {
 	//return ((int32_t)std::ceil(((float)(attackSkill * (attackValue * 0.0425) + (attackValue * 0.2)) / attackFactor)) * 2);
-	return (int32_t)std::ceil(2 * (attackValue * 2. * (attackSkill + 5.8) / 100.0 + level / 10. - 0.1));
+	return (int32_t)std::ceil((2 * (attackValue * (attackSkill + 5.8) / 25 + (level - 1) / 10.)) / attackFactor);
 }
 
 Weapon::Weapon(LuaScriptInterface* _interface) :
