06ac0d19d263b6cb12e8438e48acae42c3dad478
Dawid Pietrzak <fightingelf@gmail.com>
2008-11-30 06:51:48 +0000

    * argh
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@666 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- spells.cpp
+++ spells.cpp
@@ -210,7 +210,7 @@ InstantSpell* Spells::getInstantSpell(const std::string words)
 		if(param[0] != ' ')
 			return NULL;
 
-		if(param.length() > 1 && ((param.find(' ', 1) == std::string::npos && !result->getHasParam()) || param[1] != '"')))
+		if(param.length() > 1 && (param[1] != '"' || (param.find(' ', 1) == std::string::npos && !result->getHasParam())))
 			return NULL;
 	}
 
