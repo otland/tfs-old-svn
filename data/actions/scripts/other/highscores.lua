c8ac4ae0d07e9543c59a1ffa0679011ad135e0c8
Dawid Pietrzak <fightingelf@gmail.com>
2008-11-30 21:51:08 +0000

    * Yet before commit to public svn, included in pre-compiled packages
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@678 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- data/actions/scripts/other/highscores.lua
+++ data/actions/scripts/other/highscores.lua
@@ -1,5 +1,5 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	if(item.actionid >= 150 && item.actionid <= 158) then
+	if(item.actionid >= 150 and item.actionid <= 158) then
 		doShowTextDialog(cid, item.itemid, getHighscoreString((item.actionid - 150)))
 		return TRUE
 	end
