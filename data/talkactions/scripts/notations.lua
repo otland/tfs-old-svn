80f3f6e5729691b386355b91e55aee9f58b38c0b
Dawid Pietrzak <fightingelf@gmail.com>
2008-11-30 04:03:17 +0000

    * Fixed /notations command
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@656 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- data/talkactions/scripts/notations.lua
+++ data/talkactions/scripts/notations.lua
@@ -1,30 +1,31 @@
 function onSay(cid, words, param)
-	local t = string.explode(param, ",")
-	t[1] = tonumber(t[1])
-	if(t[1] == nil) then
+	local ret = tonumber(param)
+	if(ret == nil) then
+		local t = string.explode(param, ",")
 		if(t[2] and getBooleanFromString(t[2]) == TRUE) then
-			t[1] = getAccountIdByName(param)
+			ret = getAccountIdByName(t[1])
 		else
-			t[1] = getAccountIdByAccount(param)
+			ret = getAccountIdByAccount(t[1])
 		end
 	end
 
-	if(t[1] <= 0) then
+	if(ret <= 0) then
 		doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "Invalid account data or player name.")
 		return TRUE
 	end
 
-	local list = getBanList(BANTYPE_NOTATION, tmp)
+	local list = getBanList(BANTYPE_NOTATION, ret)
 	if(type(list) ~= "table" or table.maxn(list) <= 0) then
-		doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "Account " .. t[1] .. " does not have any notation.")
+		doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "Such account or player does not have any notation.")
 		return TRUE
 	end
 
-	local tmp = "Notations for account: " .. t[1] .. "\n"
+	ret = "Notations for account " .. ret .. "\n"
 	for i, ban in ipairs(list) do
-		tmp = "\nAdded at " .. os.date("%c", ban.added) .. " by " .. getPlayerNameByGUID(ban.adminId) .. " with reason: " .. getBanReason(ban.reason) .. " and comment: " .. ban.comment .. "."
+		local tmp = ban.adminId ~= 0 and getPlayerNameByGUID(ban.adminId) or "unknown"
+		ret = ret .. "\nAdded at " .. os.date("%c", ban.added) .. " by " .. tmp .. " with reason: " .. getBanReason(ban.reason) .. " and comment: " .. ban.comment .. "."
 	end
 
-	doPlayerPopupFYI(cid, tmp)
+	doPlayerPopupFYI(cid, ret)
 	return TRUE
 end
