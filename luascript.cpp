352b8e5e350a4c773b84ae11916c78d2e32a2747
Dawid Pietrzak <fightingelf@gmail.com>
2008-12-02 02:53:53 +0000

    * Woah, compiles ;D Ok, now we should accept negative values.
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@694 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- luascript.cpp
+++ luascript.cpp
@@ -8040,7 +8040,7 @@ int32_t LuaScriptInterface::luaAddEvent(lua_State* L)
 
 	eventDesc.parameters = params;
 
-	uint32_t delay = std::max((uint64_t)100, popNumber(L));
+	uint32_t delay = std::max((int64_t)100, popNumber(L));
 	eventDesc.function = luaL_ref(L, LUA_REGISTRYINDEX);
 
 	eventDesc.scriptId = env->getScriptId();
