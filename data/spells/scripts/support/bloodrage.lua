59d5753a909a37a610f3628800ee1c9bbe3cf2ca
Dawid Pietrzak <fightingelf@gmail.com>
2008-12-11 16:01:49 +0000

    * Protector, Sharpshooter and Bloodrage spells ;)
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@807 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- data/spells/scripts/support/bloodrage.lua
+++ data/spells/scripts/support/bloodrage.lua
@@ -4,18 +4,14 @@ setCombatParam(combat, COMBAT_PARAM_AGGRESSIVE, 0)
 
 local condition = createConditionObject(CONDITION_ATTRIBUTES)
 setConditionParam(condition, CONDITION_PARAM_TICKS, 10000)
-setConditionParam(condition, CONDITION_PARAM_SKILL_SHIELDPERCENT, 220)
+setConditionParam(condition, CONDITION_PARAM_SKILL_MELEEPERCENT, 135)
 setConditionParam(condition, CONDITION_PARAM_BUFF, TRUE)
 setCombatCondition(combat, condition)
 
-local disable = createConditionObject(CONDITION_DISABLE_ATTACK)
+local disable = createConditionObject(CONDITION_DISABLE_DEFENSE)
 setConditionParam(disable, CONDITION_PARAM_TICKS, 10000)
-
-local exhaust = createConditionObject(CONDITION_EXHAUST_COMBAT)
-setConditionParam(exhaust, CONDITION_PARAM_TICKS, 10000)
 
 function onCastSpell(cid, var)
 	doAddCondition(cid, disable)
-	doAddCondition(cid, exhaust)
 	return doCombat(cid, combat, var)
 end
--- data/spells/scripts/support/sharpshooter.lua
+++ data/spells/scripts/support/sharpshooter.lua
@@ -5,18 +5,22 @@ setCombatParam(combat, COMBAT_PARAM_AGGRESSIVE, 0)
 local condition = createConditionObject(CONDITION_ATTRIBUTES)
 setConditionParam(condition, CONDITION_PARAM_TICKS, 10000)
 setConditionParam(condition, CONDITION_PARAM_SKILL_DISTANCEPERCENT, 150)
+setConditionParam(condition, CONDITION_PARAM_BUFF, TRUE)
 setCombatCondition(combat, condition)
 
 local speed = createConditionObject(CONDITION_PARALYZE)
 setConditionParam(speed, CONDITION_PARAM_TICKS, 10000)
-setConditionParam(speed, CONDITION_PARAM_BUFF, TRUE)
 setConditionFormula(speed, -0.7, 56, -0.7, 56)
 
-local block = createConditionObject(CONDITION_DISABLE_DEFENSE)
-setConditionParam(block, CONDITION_PARAM_TICKS, 10000)
+local disable = createConditionObject(CONDITION_DISABLE_DEFENSE)
+setConditionParam(disable, CONDITION_PARAM_TICKS, 10000)
+
+local exhaust = createConditionObject(CONDITION_EXHAUST_HEAL)
+setConditionParam(exhaust, CONDITION_PARAM_TICKS, 10000)
 
 function onCastSpell(cid, var)
+	doAddCondition(cid, disable)
+	doAddCondition(cid, exhaust)
 	doAddCondition(cid, speed)
-	doAddCondition(cid, block)
 	return doCombat(cid, combat, var)
 end
