4cc335d58f386b3d97fd87f0a7a3f4716d36a124
Dawid Pietrzak <fightingelf@gmail.com>
2008-12-16 21:18:50 +0000

    * FUCK THIS
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@885 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- luascript.cpp
+++ luascript.cpp
@@ -7274,7 +7274,7 @@ int32_t LuaScriptInterface::luaGetAccountByAccountId(lua_State *L)
 	//getAccountByAccountId(accId)
 	std::string value = 0;
 	IOLoginData::getInstance()->getAccountName(popNumber(L), value);
-	lua_pushstring(L, value);
+	lua_pushstring(L, value.c_str());
 	return 1;
 }
 
