6f6c46399d0d398e93b502f5ad420a51c84324e0
Dawid Pietrzak <fightingelf@gmail.com>
2009-01-02 15:55:11 +0000

    * Fixes
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@1051 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- data/spells/scripts/support/blood rage.lua
+++ data/spells/scripts/support/blood rage.lua
@@ -5,12 +5,9 @@ setCombatParam(combat, COMBAT_PARAM_AGGRESSIVE, 0)
 local condition = createConditionObject(CONDITION_ATTRIBUTES)
 setConditionParam(condition, CONDITION_PARAM_TICKS, 10000)
 setConditionParam(condition, CONDITION_PARAM_SKILL_MELEEPERCENT, 135)
+setConditionParam(condition, CONDITION_PARAM_SKILL_SHIELDPERCENT, -100)
 setConditionParam(condition, CONDITION_PARAM_BUFF, TRUE)
 setCombatCondition(combat, condition)
-
-local disable = createConditionObject(CONDITION_DISABLE_DEFENSE)
-setConditionParam(disable, CONDITION_PARAM_TICKS, 10000)
-setCombatCondition(combat, disable)
 
 function onCastSpell(cid, var)
 	return doCombat(cid, combat, var)
--- data/spells/scripts/support/swift foot.lua
+++ data/spells/scripts/support/swift foot.lua
@@ -7,7 +7,7 @@ setConditionParam(condition, CONDITION_PARAM_TICKS, 10000)
 setConditionFormula(condition, 0.8, -72, 0.8, -72)
 setCombatCondition(combat, condition)
 
-local disable = createConditionObject(CONDITION_DISABLE_ATTACK)
+local disable = createConditionObject(CONDITION_PACIFIED)
 setConditionParam(disable, CONDITION_PARAM_TICKS, 10000)
 setCombatCondition(combat, disable)
 
