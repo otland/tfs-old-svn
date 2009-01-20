087d4dd6d0e8b55e1dae1caa18baff65eb83ebf9
Dawid Pietrzak <fightingelf@gmail.com>
2009-01-20 01:23:03 +0000

    * 0.3b3 final tagging, release
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@1102 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- data/movements/scripts/citizen.lua
+++ data/movements/scripts/citizen.lua
@@ -1,5 +1,5 @@
 function onStepIn(cid, item, position, fromPosition)
-	if item.actionid > 30020 and item.actionid < 30100 then
+	if(item.actionid > 30020 and item.actionid < 30100) then
 		doPlayerSetTown(cid, item.actionid - 30020)
 	end
 	return TRUE
