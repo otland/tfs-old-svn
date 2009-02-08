9a32e2a208015eef65fa344d8673aa51676a6628
Dawid Pietrzak <fightingelf@gmail.com>
2009-02-08 18:45:25 +0000

    * And this (these are included in packages im doing)
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@1206 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- data/talkactions/scripts/uptime.lua
+++ data/talkactions/scripts/uptime.lua
@@ -1,10 +1,12 @@
 function onSay(cid, words, param)
-	local hours = math.ceil(getWorldUpTime() / 3600) - 1
-	local minutes = math.ceil((getWorldUpTime() - (3600 * hours)) / 60)
-	if minutes == 60 then
+	local tmp = getWorldUpTime()
+	local hours = math.ceil(tmp / 3600) - 1
+	local minutes = math.ceil((tmp - (3600 * hours)) / 60)
+	if(minutes == 60) then
 		minutes = 0
 		hours = hours + 1
 	end
+
 	doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "Uptime: " .. hours .. " hours and " .. minutes .. " minutes.")
 	return TRUE
 end
