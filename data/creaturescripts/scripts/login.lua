e5b1a1ab83c2cbb6da79639453c5acb073c565fa
Dawid Pietrzak <fightingelf@gmail.com>
2009-02-08 18:41:30 +0000

    * Almost epic fail ;D
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@1205 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- data/creaturescripts/scripts/login.lua
+++ data/creaturescripts/scripts/login.lua
@@ -1,8 +1,9 @@
 function onLogin(cid)
 	local loss = getConfigValue('deathLostPercent')
 	if(loss ~= nil) then
-	for i = PLAYERLOSS_EXPERIENCE, PLAYERLOSS_ITEMS do
-		doPlayerSetLossPercent(cid, i, getConfigValue('deathLostPercent'))
+		for i = PLAYERLOSS_EXPERIENCE, PLAYERLOSS_ITEMS do
+			doPlayerSetLossPercent(cid, i, getConfigValue('deathLostPercent'))
+		end
 	end
 
 	registerCreatureEvent(cid, "PlayerDeath")
