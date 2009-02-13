9382f297301cbd43e17c7c72065ba08b2f913ffe
Dawid Pietrzak <fightingelf@gmail.com>
2009-02-13 12:00:41 +0000

    * Added message for /squelch and /cliport
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@1247 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- data/talkactions/scripts/gamemaster.lua
+++ data/talkactions/scripts/gamemaster.lua
@@ -4,16 +4,21 @@ local teleport = createConditionObject(CONDITION_GAMEMASTER, -1, FALSE, GAMEMAST
 function onSay(cid, words, param)
 	local condition = ignore
 	local subId = GAMEMASTER_IGNORE
+	local name = "private messages ignoring"
 	if(words:sub(2, 2) == "c") then
 		condition = teleport
 		subId = GAMEMASTER_TELEPORT
+		name = "map click teleport"
 	end
 
+	local action = "off"
 	if(getCreatureCondition(cid, CONDITION_GAMEMASTER, subId) ~= TRUE) then
 		doAddCondition(cid, condition)
+		action = "on"
 	else
 		doRemoveCondition(cid, CONDITION_GAMEMASTER, subId)
 	end
 
+	doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "You have turned " .. action .. " " .. name .. ".")
 	return TRUE
 end
