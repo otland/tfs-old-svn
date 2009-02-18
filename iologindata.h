09f7b7da1e1ba0027d77be5f06bcccee464d7aff
Dawid Pietrzak <fightingelf@gmail.com>
2009-02-18 12:11:01 +0000

    * ...2
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@1292 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- iologindata.h
+++ iologindata.h
@@ -118,6 +118,14 @@ class IOLoginData
 		bool resetGuildInformation(uint32_t guid);
 
 	protected:
+		struct StringCompareCase
+		{
+			bool operator()(const std::string& l, const std::string& r) const
+			{
+				return strcasecmp(l.c_str(), r.c_str()) < 0;
+			}
+		};
+
 		typedef std::map<int32_t, std::pair<Item*, int32_t> > ItemMap;
 		typedef std::map<uint32_t, std::string> NameCacheMap;
 		typedef std::map<std::string, uint32_t, StringCompareCase> GuidCacheMap;
