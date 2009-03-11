35a9d945ad1bd7eb04d4cd322974619ce40c20db
Dawid Pietrzak <fightingelf@gmail.com>
2009-03-11 01:08:29 +0000

    * Emergency abs...
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@1349 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- player.cpp
+++ player.cpp
@@ -1393,7 +1393,7 @@ void Player::onCreatureAppear(const Creature* creature, bool isLogin)
 		{
 			int32_t period = (int32_t)time(NULL) - lastLogout - 600;
 			if(period > 0)
-				useStamina(-std::min(getSpentStamina(), (uint64_t)period * g_config.getNumber(ConfigManager::RATE_STAMINA_GAIN)));
+				useStamina(-std::min(getSpentStamina(), (uint64_t)std::abs(period * g_config.getNumber(ConfigManager::RATE_STAMINA_GAIN))));
 		}
 
 		g_game.checkPlayersRecord();
