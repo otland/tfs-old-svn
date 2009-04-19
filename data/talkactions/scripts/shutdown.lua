f56ae094065fa43145643875dda11b185d6844a6
slawkens <slawkens>
2009-04-19 15:38:56 +0000

    * Some changes in /shutdown, accepting now empty param (Server will immediately close), and 'stop' param
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@1607 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- data/talkactions/scripts/shutdown.lua
+++ data/talkactions/scripts/shutdown.lua
@@ -1,8 +1,25 @@
+local shutdownEvent = 0
+
 function onSay(cid, words, param)
+	if(param == "") then
+		doSetGameState(GAMESTATE_SHUTDOWN)
+		return TRUE
+	end
+
+	if(param:lower() == "stop") then
+		stopEvent(shutdownEvent)
+		shutdownEvent = 0
+		return TRUE
+	end
+
 	param = tonumber(param)
 	if(param == nil or param < 0) then
 		doPlayerSendCancel(cid, "Numeric param may not be lower than 0.")
 		return TRUE
+	end
+
+	if(shutdownEvent ~= 0) then
+		stopEvent(shutdownEvent)
 	end
 
 	prepareShutdown(math.abs(math.ceil(param)))
@@ -13,7 +30,6 @@ function prepareShutdown(minutes)
 	if(minutes <= 0) then
 		doSetGameState(GAMESTATE_SHUTDOWN)
 	else
-		addEvent(prepareShutdown, 60000, minutes - 1)
 		if(minutes == 1) then
 			doBroadcastMessage("Server is going down in " .. minutes .. " minute, please log out now!")
 		elseif(minutes <= 3) then
@@ -21,5 +37,6 @@ function prepareShutdown(minutes)
 		else
 			doBroadcastMessage("Server is going down in " .. minutes .. " minutes.")
 		end
+		shutdownEvent = addEvent(prepareShutdown, 60000, minutes - 1)
 	end
 end
