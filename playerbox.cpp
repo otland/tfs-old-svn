40b7fb1d9d9bb92b8a59744aa2ef57fb1a17d553
Dawid Pietrzak <fightingelf@gmail.com>
2009-04-20 04:32:49 +0000

    * omg...
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@1636 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- playerbox.cpp
+++ playerbox.cpp
@@ -17,9 +17,8 @@
 // along with this program; if not, write to the Free Software Foundation,
 // Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 //////////////////////////////////////////////////////////////////////
-
-#ifndef __CONSOLE__
 #include "otpch.h"
+#ifndef __CONSOLE__
 #include "gui.h"
 
 #include "game.h"
