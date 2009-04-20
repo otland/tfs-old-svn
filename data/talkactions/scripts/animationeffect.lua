bd1678ee04a5f9ef344dffcfd102778314165498
Dawid Pietrzak <fightingelf@gmail.com>
2009-04-20 02:07:13 +0000

    asdf
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@1633 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- data/talkactions/scripts/animationeffect.lua
+++ data/talkactions/scripts/animationeffect.lua
@@ -6,10 +6,8 @@ function onSay(cid, words, param)
 	end
 
 	local position = getCreaturePosition(cid)
-	local i = 0
-	while i <= 30 do
+	for i = 1, 30 do
 		doSendDistanceShoot(position, {x = position.x + math.random(-7, 7), y = position.y + math.random(-5, 5), z = position.z}, param)
-		i = i + 1
 	end
 
 	return TRUE
