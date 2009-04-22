ecafaf22b2abed6f90cea6dadee01b3ce8a5bc2d
Dawid Pietrzak <fightingelf@gmail.com>
2009-04-22 21:35:04 +0000

    * Fixed /owner talkaction
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@1689 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- data/talkactions/scripts/owner.lua
+++ data/talkactions/scripts/owner.lua
@@ -1,3 +1,4 @@
+local NO_OWNER_PHRASE = {"none", "nobody", "0"}
 function onSay(cid, words, param, channel)
 	if(param == "") then
 		doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "Command requires param.")
@@ -10,11 +11,22 @@ function onSay(cid, words, param, channel)
 		clean = getBooleanFromString(t[2])
 	end
 
-	local guid = getPlayerGUIDByName(t[1])
-	local hid = getTileHouseInfo(getCreaturePosition(cid))
-	if(hid and guid) then
-		setHouseOwner(hid, guid, clean)
+	local guid = 0
+	if(not table.isStrIn(NO_OWNER_PHRASE, t[2]:lower())) then
+		guid = getPlayerGUIDByName(t[1])
 	end
 
+	if(not guid) then
+		doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "Player not found.")
+		return TRUE
+	end
+
+	local hid = getTileHouseInfo(getCreaturePosition(cid))
+	if(not hid) then
+		doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "You are not in a house.")
+		return TRUE
+	end
+
+	setHouseOwner(hid, guid, clean)
 	return TRUE
 end
