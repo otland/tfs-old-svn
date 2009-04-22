95ad775483b9f8bdc8d43ef242e75d1abe2673dd
Dawid Pietrzak <fightingelf@gmail.com>
2009-04-22 02:01:54 +0000

    * Forgot about this fix
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@1678 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- spells.cpp
+++ spells.cpp
@@ -1082,9 +1082,8 @@ bool InstantSpell::canThrowSpell(const Creature* creature, const Creature* targe
 
 	const Position& fromPos = creature->getPosition();
 	const Position& toPos = target->getPosition();
-	return (!(fromPos.z != toPos.z ||
-	(range == -1 && !g_game.canThrowObjectTo(fromPos, toPos, checkLineOfSight)) ||
-	(range != -1 && !g_game.canThrowObjectTo(fromPos, toPos, checkLineOfSight, range, range))));
+	return (!(fromPos.z != toPos.z || (range == -1 && !g_game.canThrowObjectTo(fromPos, toPos, checkLineOfSight))
+		|| (range != -1 && !g_game.canThrowObjectTo(fromPos, toPos, checkLineOfSight, range, range))));
 }
 
 bool InstantSpell::castSpell(Creature* creature)
