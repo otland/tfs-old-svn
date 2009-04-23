e1e2091f3766a58d62d4fec963e24a480b514cfb
lithium <lithium>
2009-04-23 05:06:27 +0000

    * Typo~
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@1695 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- otserv.cpp
+++ otserv.cpp
@@ -388,7 +388,7 @@ ServiceManager* services)
 	#endif
 
 	std::cout << STATUS_SERVER_NAME << ", version " << STATUS_SERVER_VERSION << " (" << STATUS_SERVER_CODENAME << ")" << std::endl;
-	std::cout << "A server developed by Elf, Talaturen, lithium, Kiper, Kornholijo, KaczooH, slawkens & Macroman." << std::endl;
+	std::cout << "A server developed by Elf, Talaturen, Lithium, Kiper, Kornholijo, KaczooH, slawkens & Macroman." << std::endl;
 	std::cout << "Visit our forum for updates, support and resources: http://otland.net." << std::endl;
 	std::cout << std::endl;
 
@@ -916,7 +916,7 @@ LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT message, WPARAM wParam, LPARAM 
 						GUI::getInstance()->m_logText = "";
 						GUI::getInstance()->m_lineCount = 0;
 						std::cout << STATUS_SERVER_NAME << ", version " << STATUS_SERVER_VERSION << " (" << STATUS_SERVER_CODENAME << ")" << std::endl;
-						std::cout << "A server developed by Elf, Talaturen, lithium, Kiper, Kornholijo, KaczooH, slawkens & Macroman." << std::endl;
+						std::cout << "A server developed by Elf, Talaturen, Lithium, Kiper, Kornholijo, KaczooH, slawkens & Macroman." << std::endl;
 						std::cout << "Visit our forum for updates, support and resources: http://otland.net." << std::endl;
 						std::cout << std::endl;
 					}
