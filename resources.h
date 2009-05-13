c13599a375ce3844d421f4d871a65ad6e4967e0a
Dawid Pietrzak <fightingelf@gmail.com>
2009-05-13 22:45:57 +0000

    * NOW PL2!!!!11
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@1871 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- resources.h
+++ resources.h
@@ -66,6 +66,6 @@
 
 #define VERSION_CHECK "http://forgottenserver.otland.net/version.xml"
 #define VERSION_PATCH 2
-#define VERSION_TIMESTAMP 1242125863
-#define VERSION_BUILD 1859
+#define VERSION_TIMESTAMP 1242254821
+#define VERSION_BUILD 1871
 #define VERSION_DATABASE 13
