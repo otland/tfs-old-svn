3649aa5c28ced95d923521e440fb5972eef103c8
Dawid Pietrzak <fightingelf@gmail.com>
2009-05-21 07:20:22 +0000

    * Fishing from PZ gives no effect, player advances only if he catches the fish
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@1970 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- data/actions/scripts/tools/fishing.lua
+++ data/actions/scripts/tools/fishing.lua
@@ -1,21 +1,20 @@
 local config = {
+	waterIds = {493, 4608, 4609, 4610, 4611, 4612, 4613, 4614, 4615, 4616, 4617, 4618, 4619, 4620, 4621, 4622, 4623, 4624, 4625},
+	rateSkill = getConfigValue("rateSkill"),
+	allowFromPz = false,
 	useWorms = true
 }
 
-local waterIds = {493, 4608, 4609, 4610, 4611, 4612, 4613, 4614, 4615, 4616, 4617, 4618, 4619, 4620, 4621, 4622, 4623, 4624, 4625}
-
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	if(not isInArray(waterIds, itemEx.itemid)) then
+	if(not isInArray(config.waterIds, itemEx.itemid)) then
 		return false
 	end
 
-	if(itemEx.itemid ~= 493) then
-		if(math.random(1, (100 + (getPlayerSkill(cid, SKILL_FISHING) / 10))) <= getPlayerSkill(cid, SKILL_FISHING) and
-			(not config.useWorms or (getPlayerItemCount(cid, ITEM_WORM) > 0 and doPlayerRemoveItem(cid, ITEM_WORM, 1))))
-		then
-				doPlayerAddItem(cid, ITEM_FISH, 1)
-		end
-		doPlayerAddSkillTry(cid, SKILL_FISHING, 1)
+	if((config.allowFromPz or not getTileInfo(fromPosition).protection) and itemEx.itemid ~= 493 and
+		math.random(1, (100 + (getPlayerSkill(cid, SKILL_FISHING) / 10))) <= getPlayerSkill(cid, SKILL_FISHING) and
+		(not config.useWorms or (getPlayerItemCount(cid, ITEM_WORM) > 0 and doPlayerRemoveItem(cid, ITEM_WORM, 1)))) then
+		doPlayerAddItem(cid, ITEM_FISH, 1)
+		doPlayerAddSkillTry(cid, SKILL_FISHING, rateSkill)
 	end
 
 	doSendMagicEffect(toPosition, CONST_ME_LOSEENERGY)
