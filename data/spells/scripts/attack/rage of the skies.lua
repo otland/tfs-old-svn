bdbe0a7c34a870c35bc3920f52aa5c830eeeeb47
slawkens <slawkens>
2009-06-12 09:38:30 +0000

    * Player flags uppercase as in OpenTibia (For compatibility) * Using more vocation ids instead names (Name should be only used in vocations.xml file, so its easy to change) * Started reviewing monsters, fixed hp, exp, flags, loot etc (Loot according to fe. http://tibia.wikia.com/wiki/Amazon/Loot_Statistics), done Amazons & Annelids * Rage of the Skies range = 6 (From OpenTibia)
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@2178 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- data/spells/scripts/attack/rage of the skies.lua
+++ data/spells/scripts/attack/rage of the skies.lua
@@ -3,7 +3,7 @@ setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_ENERGYDAMAGE)
 setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_BIGCLOUDS)
 setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -1.3, -30, -1.6, 0)
 
-local area = createCombatArea(AREA_CROSS5X5)
+local area = createCombatArea(AREA_CROSS6X6)
 setCombatArea(combat, area)
 
 function onCastSpell(cid, var)
