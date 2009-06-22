d16ff4105b66a54c773cf3380736cf4db831cb34
slawkens <slawkens>
2009-06-22 20:07:21 +0000

    * Potions can 'splash' now ;)
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@2272 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- data/actions/scripts/liquids/potions.lua
+++ data/actions/scripts/liquids/potions.lua
@@ -1,22 +1,24 @@
 local config = {
 	removeOnUse = "no",
+	splashable = "yes",
 	healthMultiplier = 1.0,
 	manaMultiplier = 1.0
 }
 
 config.removeOnUse = getBooleanFromString(config.removeOnUse)
+config.splashable = getBooleanFromString(config.splashable)
 local POTIONS = {
-	[8704] = {empty = 7636, health = {50, 100}}, -- small health potion
-	[7618] = {empty = 7636, health = {100, 200}}, -- health potion
-	[7588] = {empty = 7634, health = {200, 400}, level = 50, vocations = {3, 4, 7, 8}, vocStr = "knights and paladins"}, -- strong health potion
-	[7591] = {empty = 7635, health = {500, 700}, level = 80, vocations = {4, 8}, vocStr = "knights"}, -- great health potion
-	[8473] = {empty = 7635, health = {800, 1000}, level = 130, vocations = {4, 8}, vocStr = "knights"}, -- ultimate health potion
+	[8704] = {empty = 7636, splash = 2, health = {50, 100}}, -- small health potion
+	[7618] = {empty = 7636, splash = 2, health = {100, 200}}, -- health potion
+	[7588] = {empty = 7634, splash = 2, health = {200, 400}, level = 50, vocations = {3, 4, 7, 8}, vocStr = "knights and paladins"}, -- strong health potion
+	[7591] = {empty = 7635, splash = 2, health = {500, 700}, level = 80, vocations = {4, 8}, vocStr = "knights"}, -- great health potion
+	[8473] = {empty = 7635, splash = 2, health = {800, 1000}, level = 130, vocations = {4, 8}, vocStr = "knights"}, -- ultimate health potion
 
-	[7620] = {empty = 7636, mana = {70, 130}}, -- mana potion
-	[7589] = {empty = 7634, mana = {110, 190}, level = 50, vocations = {1, 2, 3, 5, 6, 7}, vocStr = "sorcerers, druids and paladins"}, -- strong mana potion
-	[7590] = {empty = 7635, mana = {200, 300}, level = 80, vocations = {1, 2, 5, 6}, vocStr = "sorcerers and druids"}, -- great mana potion
+	[7620] = {empty = 7636, splash = 7, mana = {70, 130}}, -- mana potion
+	[7589] = {empty = 7634, splash = 7, mana = {110, 190}, level = 50, vocations = {1, 2, 3, 5, 6, 7}, vocStr = "sorcerers, druids and paladins"}, -- strong mana potion
+	[7590] = {empty = 7635, splash = 7, mana = {200, 300}, level = 80, vocations = {1, 2, 5, 6}, vocStr = "sorcerers and druids"}, -- great mana potion
 
-	[8472] = {empty = 7635, health = {200, 400}, mana = {110, 190}, level = 80, vocations = {3, 7}, vocStr = "paladins"} -- great spirit potion
+	[8472] = {empty = 7635, splash = 3, health = {200, 400}, mana = {110, 190}, level = 80, vocations = {3, 7}, vocStr = "paladins"} -- great spirit potion
 }
 
 local exhaust = createConditionObject(CONDITION_EXHAUST)
@@ -24,8 +26,22 @@ setConditionParam(exhaust, CONDITION_PARAM_TICKS, (getConfigInfo('timeBetweenExA
 
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 	local potion = POTIONS[item.itemid]
-	if(not potion or not isPlayer(itemEx.uid)) then
+	if(not potion) then
 		return false
+	end
+
+	if(not isPlayer(itemEx.uid)) then
+		if(not config.splashable) then
+			return false
+		end
+
+		if(toPosition.x == CONTAINER_POSITION) then
+			toPosition = getThingPos(item.uid)
+		end
+
+		doDecayItem(doCreateItem(2016, potion.splash, toPosition))
+		doTransformItem(item.uid, potion.empty)
+		return true
 	end
 
 	if(hasCondition(cid, CONDITION_EXHAUST_HEAL)) then
