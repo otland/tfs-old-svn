eea01c58e79361b40709530d1d3f6b744714df2a
slawkens <slawkens>
2009-06-23 11:51:40 +0000

    * [Draw Well] According to wiki: 'Sometimes it leads to a room under it'. So now you need to set actionid = 100 to make it teleportable. Probably much of you had same as me problem with blocked players under town....
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@2275 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- data/actions/scripts/other/teleport.lua
+++ data/actions/scripts/other/teleport.lua
@@ -1,5 +1,11 @@
 local UP_FLOORS = {1386, 3678, 5543, 8599}
+local DRAW_WELL = 1369
+
 function onUse(cid, item, fromPosition, itemEx, toPosition)
+	if(item.itemid == DRAW_WELL and item.actionid ~= 100) then
+		return false
+	end
+
 	if(isInArray(UP_FLOORS, item.itemid)) then
 		fromPosition.y = fromPosition.y + 1
 		fromPosition.z = fromPosition.z - 1
