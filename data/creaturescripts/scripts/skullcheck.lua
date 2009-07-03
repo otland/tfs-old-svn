449748dcf74af4e8fb9dfa1f71a7356e168e97c3
slawkens <slawkens>
2009-07-03 09:09:26 +0000

    * Fix
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@2319 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- data/creaturescripts/scripts/skullcheck.lua
+++ data/creaturescripts/scripts/skullcheck.lua
@@ -1,6 +1,6 @@
 function onThink(cid, interval)
-	local skull, skullEnd = getCreatureSkull(cid), getPlayerRedSkullEnd(cid)
+	local skull, skullEnd = getCreatureSkull(cid), getPlayerSkullEnd(cid)
 	if(skullEnd > 0 and skull > SKULL_WHITE and os.time() > skullEnd and not getCreatureCondition(cid, CONDITION_INFIGHT)) then
-		doPlayerSetRedSkullEnd(cid, 0, skull)
+		doPlayerSetSkullEnd(cid, 0, skull)
 	end
 end
