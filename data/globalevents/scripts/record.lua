5a4100ab1c18f3e76235b1a5ce32b21f29d82c0a
slawkens <slawkens>
2009-07-19 17:36:27 +0000

    * ...
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@2413 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- data/globalevents/scripts/record.lua
+++ data/globalevents/scripts/record.lua
@@ -1,4 +1,4 @@
 function onPlayersRecord(new, old, cid)
 	db.executeQuery("INSERT INTO `server_record` (`record`, `world_id`, `timestamp`) VALUES (" .. new .. ", " .. getConfigValue('worldId') .. ", " .. os.time() .. ");")
-	addEvent(doBroadcastMessage, 150, "New record: " .. newRecord .. " players are logged in.", MESSAGE_STATUS_DEFAULT)
+	addEvent(doBroadcastMessage, 150, "New record: " .. new .. " players are logged in.", MESSAGE_STATUS_DEFAULT)
 end
