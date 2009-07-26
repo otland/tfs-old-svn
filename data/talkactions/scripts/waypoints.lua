fc795e085f5728c46e7bf40ab58604a566a0eb3c
slawkens <slawkens>
2009-07-26 00:21:55 +0000

    * Renamed /waypoints to /wp * You can now save temporial waypoint by using '/wp waypointName' (And next teleport to it by using /goto waypointName)
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@2452 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- data/talkactions/scripts/waypoints.lua
+++ data/talkactions/scripts/waypoints.lua
@@ -1,4 +1,10 @@
 function onSay(cid, words, param, channel)
+	if(param:trim() ~= '') then
+		doWaypointAddTemporial(param, getCreaturePosition(cid))
+		doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "Temporial waypoint saved.")
+		return true
+	end
+
 	local str = ""
 	for i, waypoint in ipairs(getWaypointList()) do
 		str = str .. waypoint.name .. "\n"
