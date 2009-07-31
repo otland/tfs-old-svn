9e8aa50d5dbdb166543e653eb904b68b2359b25c
Dawid Pietrzak <fightingelf@gmail.com>
2009-07-31 02:58:39 +0000

    * Whoops, forgot this one ;_)!
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@2518 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- game.h
+++ game.h
@@ -435,9 +435,9 @@ class Game
 		//Implementation of player invoked events
 		bool playerBroadcastMessage(Player* player, SpeakClasses type, const std::string& text);
 		bool playerReportBug(uint32_t playerId, std::string bug);
-		bool playerViolationWindow(uint32_t playerId, const std::string& targetName, uint8_t reason,
-			ViolationAction_t action, const std::string& comment, uint32_t statementId,
-			uint16_t channelId, bool ipBanishment);
+		bool playerViolationWindow(uint32_t playerId, std::string name, uint8_t reason,
+			ViolationAction_t action, const std::string& comment, std::string statement,
+			uint32_t statementId, bool ipBanishment);
 		bool playerMoveThing(uint32_t playerId, const Position& fromPos, uint16_t spriteId,
 			int16_t fromStackpos, const Position& toPos, uint8_t count);
 		bool playerMoveCreature(uint32_t playerId, uint32_t movingCreatureId,
