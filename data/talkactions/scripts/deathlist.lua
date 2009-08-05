3099f786d48855b94b27d7a6e69a350fd41f6e9e
Dawid Pietrzak <fightingelf@gmail.com>
2009-08-05 12:14:15 +0000

    * Fixed deathlist...
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@2534 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- data/talkactions/scripts/deathlist.lua
+++ data/talkactions/scripts/deathlist.lua
@@ -1,10 +1,10 @@
 local config = {
-	deathAssistCount = getConfigValue('deathAssistCount'),
+	deathAssistCount = getConfigValue('deathAssistCount') + 1,
 	maxDeathRecords = getConfigValue('maxDeathRecords'),
 	limit = ""
 }
-if(config.deathAssistCount > -1) then
-	config.limit = " LIMIT 0, " .. config.deathAssistCount + 1
+if(config.deathAssistCount > 0) then
+	config.limit = " LIMIT 0, " .. config.deathAssistCount
 end
 
 function onSay(cid, words, param, channel)
@@ -14,12 +14,10 @@ function onSay(cid, words, param, channel)
 		return true
 	end
 
-	local targetName = target:getDataString("name")
-	local targetId = target:getDataInt("id")
+	local targetName, targetId = target:getDataString("name"), target:getDataInt("id")
 	target:free()
 
-	local str = ""
-	local deaths = db.getResult("SELECT `id`, `date`, `level` FROM `player_deaths` WHERE `player_id` = " .. targetId .." ORDER BY `date` DESC LIMIT 0, " .. config.maxDeathRecords)
+	local str, deaths = "", db.getResult("SELECT `id`, `date`, `level` FROM `player_deaths` WHERE `player_id` = " .. targetId .." ORDER BY `date` DESC LIMIT 0, " .. config.maxDeathRecords)
 	if(deaths:getID() ~= -1) then
 		repeat
 			local killers = db.getResult("SELECT environment_killers.name AS monster_name, players.name AS player_name FROM killers LEFT JOIN environment_killers ON killers.id = environment_killers.kill_id LEFT JOIN player_killers ON killers.id = player_killers.kill_id LEFT JOIN players ON players.id = player_killers.player_id WHERE killers.death_id = " .. deaths:getDataInt("id") .. " ORDER BY killers.final_hit DESC, killers.id ASC" .. config.limit)
@@ -30,20 +28,20 @@ function onSay(cid, words, param, channel)
 					str = os.date("%d %B %Y %X ", deaths:getDataLong("date"))
 				end
 
-				local count = killers:getRows(false)
-				local i = 0
+				local count, i = killers:getRows(false), 0
 				repeat
 					local monster = killers:getDataString("monster_name")
 					if(i == 0 or i == (count - 1)) then
 						monster = string.gsub(monster:gsub("an ", ""), "a ", "")
 					end
 
-					i = i + 1
 					if(killers:getDataString("player_name") ~= "") then
-						if(i == 1) then
-							str = str .. "Killed at level " .. deaths:getDataInt("level") .. " by "
+						if(i == 0) then
+							str = str .. "Killed at level " .. deaths:getDataInt("level") .. " by:\n  "
 						elseif(i == count) then
 							str = str .. " and by "
+						elseif(i % 4 == 0) then
+							str = str .. ",\n  "
 						else
 							str = str .. ", "
 						end
@@ -54,10 +52,12 @@ function onSay(cid, words, param, channel)
 
 						str = str .. killers:getDataString("player_name")
 					else
-						if(i == 1) then
-							str = str .. "Died at level " .. deaths:getDataInt("level") .. " by "
+						if(i == 0) then
+							str = str .. "Died at level " .. deaths:getDataInt("level") .. " by:\n  "
 						elseif(i == count) then
 							str = str .. " and by "
+						elseif(i % 4 == 0) then
+							str = str .. ",\n  "
 						else
 							str = str .. ", "
 						end
@@ -65,6 +65,7 @@ function onSay(cid, words, param, channel)
 						str = str .. monster
 					end
 
+					i = i + 1
 					if(i == count) then
 						str = str .. "."
 					end
