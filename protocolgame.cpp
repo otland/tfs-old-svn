6f55b2a583c6b8abb98dac477e23f3b70d696c19
Dawid Pietrzak <fightingelf@gmail.com>
2009-08-20 21:31:50 +0000

    * A fix.
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@2618 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- protocolgame.cpp
+++ protocolgame.cpp
@@ -2462,24 +2462,17 @@ void ProtocolGame::sendOutfitWindow()
 		{
 			msg->AddByte((size_t)std::min((size_t)OUTFITS_MAX_NUMBER, outfitList.size()));
 			std::list<Outfit>::iterator it = outfitList.begin();
-			if(player->hasCustomFlag(PlayerCustomFlag_CanWearAllAddons))
+ 			for(int32_t i = 0; it != outfitList.end() && i < OUTFITS_MAX_NUMBER; ++it, ++i)
 			{
-	 			for(int32_t i = 0; it != outfitList.end() && i < OUTFITS_MAX_NUMBER; ++it, ++i)
-				{
-	 				msg->AddU16(it->lookType);
-					msg->AddString(it->name);
+ 				msg->AddU16(it->lookType);
+				msg->AddString(it->name);
+				if(player->hasCustomFlag(PlayerCustomFlag_CanWearAllAddons))
 	 				msg->AddByte(0x03);
-				}
+				else if(player->isPremium())
+					msg->AddByte(it->addons);
+				else
+					msg->AddByte(0x00);
 			}
-			else
-			{
-	 			for(int32_t i = 0; it != outfitList.end() && i < OUTFITS_MAX_NUMBER; ++it, ++i)
-				{
-	 				msg->AddU16(it->lookType);
-					msg->AddString(it->name);
-	 				msg->AddByte(it->addons);
-				}
- 			}
 		}
 		else
 		{
