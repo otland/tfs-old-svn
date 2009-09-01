c4f0120206483759e7157343b1f521dd340e28da
slawkens <slawkens>
2009-09-01 23:59:42 +0000

    ;* (commit with special dedications to Elf xD)
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@2748 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- protocolgame.cpp
+++ protocolgame.cpp
@@ -2458,8 +2458,12 @@ void ProtocolGame::sendOutfitWindow()
 			{
 				msg->AddU16(it->lookType);
 				msg->AddString(it->name);
-				msg->AddByte(player->hasCustomFlag(PlayerCustomFlag_CanWearAllAddons) ? 0x03 :
-					(player->isPremium() ? it->addons : 0x00));
+				if(player->hasCustomFlag(PlayerCustomFlag_CanWearAllAddons))
+	 				msg->AddByte(0x03);
+				else if(player->isPremium())
+					msg->AddByte(it->addons);
+				else
+					msg->AddByte(0x00);
 			}
 		}
 		else
