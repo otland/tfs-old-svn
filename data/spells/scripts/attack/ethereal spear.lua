840e6b802e63e959d02e94a8b4d03e03eb1d704a
Dawid Pietrzak <fightingelf@gmail.com>
2009-09-02 06:09:06 +0000

    * .,.
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@2762 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- data/spells/scripts/attack/ethereal spear.lua
+++ data/spells/scripts/attack/ethereal spear.lua
@@ -2,7 +2,7 @@ local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_PHYSICALDAMAGE)
 setCombatParam(combat, COMBAT_PARAM_BLOCKARMOR, true)
 setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_ETHEREALSPEAR)
-setCombatFormula(combat, COMBAT_FORMULA_SKILL, -0.33, -25, -1, -25)
+setCombatFormula(combat, COMBAT_FORMULA_SKILL, -0.33, 25, -1, 25)
 
 function onCastSpell(cid, var)
 	return doCombat(cid, combat, var)
