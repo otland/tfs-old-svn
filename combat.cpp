13f2511d76e8503d52df34ecf3e8d633d58d0e68
Dawid Pietrzak <fightingelf@gmail.com>
2009-09-02 05:20:51 +0000

    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@2758 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- combat.cpp
+++ combat.cpp
@@ -100,11 +100,11 @@ bool Combat::getMinMaxValues(Creature* creature, Creature* target, int32_t& min,
 				max = (int32_t)maxb;
 				if(Item* tool = player->getWeapon())
 				{
-					min = (int32_t)(((player->getWeaponSkill(tool) + tool->getAttack() + minb) * mina + (player->getLevel() / minl)) * multiplier);
+					min = (int32_t)((player->getWeaponSkill(tool) + tool->getAttack() + minb) * mina + (player->getLevel() / minl));
 					if(minc && std::abs(min) < std::abs(minc))
 						min = minc;
 
-					max = (int32_t)(((player->getWeaponSkill(tool) + tool->getAttack() + maxb) * maxa + (player->getLevel() / maxl)) * multiplier);
+					max = (int32_t)((player->getWeaponSkill(tool) + tool->getAttack() + maxb) * maxa + (player->getLevel() / maxl));
 					if(maxc && std::abs(max) < std::abs(maxc))
 						max = maxc;
 
