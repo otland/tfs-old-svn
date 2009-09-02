b1531b2663802f68d0376595760e026433e066f8
Dawid Pietrzak <fightingelf@gmail.com>
2009-09-02 05:37:24 +0000

    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@2759 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- data/spells/scripts/attack/fierce berserk.lua
+++ data/spells/scripts/attack/fierce berserk.lua
@@ -7,7 +7,7 @@ local area = createCombatArea(AREA_SQUARE1X1)
 setCombatArea(combat, area)
 
 function onGetFormulaValues(cid, level, skill, attack, factor)
-	local skillTotal, levelTotal = (skill + attack) * 2, level / 5
+	local skillTotal, levelTotal = skill + attack * 2, level / 5
 	return -(skillTotal * 1.1 + levelTotal), -(skillTotal * 3 + levelTotal)
 end
 
