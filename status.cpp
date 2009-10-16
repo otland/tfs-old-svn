10d36d7d7e5b65e506dfeaad586ca57d9c8373bd
Dawid Pietrzak <fightingelf@gmail.com>
2009-10-16 07:04:56 +0000

    * Reverted these changes... * Fixed issue with XML players, lol.
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/branches/0.2@2977 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- status.cpp
+++ status.cpp
@@ -52,11 +52,7 @@ enum RequestedInfo_t
 	REQUEST_MAP_INFO = 0x10,
 	REQUEST_EXT_PLAYERS_INFO = 0x20,
 	REQUEST_PLAYER_STATUS_INFO = 0x40,
-	REQUEST_SERVER_SOFTWARE_INFO = 0x80,
-	REQUEST_RATES_SERVER_INFO = 0xFF,
-
-	//TFS-specific
-	REQUEST_STAGES_SERVER_INFO = 0xFE
+	REQUEST_SERVER_SOFTWARE_INFO = 0x80
 };
 
 std::map<uint32_t, int64_t> ProtocolStatus::ipConnectMap;
@@ -254,16 +250,6 @@ void Status::getInfo(uint32_t requestedInfo, OutputMessage_ptr output, NetworkMe
 		output->AddU32((uint32_t)(running));
   	}
 
-	if(requestedInfo & REQUEST_RATES_SERVER_INFO)
-	{
-		output->AddByte(0x13);
-		output->AddU16(g_config.getNumber(ConfigManager::RATE_EXPERIENCE));
-		output->AddU16(g_config.getNumber(ConfigManager::RATE_MAGIC));
-		output->AddU16(g_config.getNumber(ConfigManager::RATE_SKILL));
-		output->AddU16(g_config.getNumber(ConfigManager::RATE_LOOT));
-		output->AddU16(g_config.getNumber(ConfigManager::RATE_SPAWN));
-	}
-
 	if(requestedInfo & REQUEST_PLAYERS_INFO)
 	{
 		output->AddByte(0x20);
@@ -282,23 +268,6 @@ void Status::getInfo(uint32_t requestedInfo, OutputMessage_ptr output, NetworkMe
 		output->AddU16(mapWidth);
 		output->AddU16(mapHeight);
   	}
-
-	if(requestedInfo & REQUEST_STAGES_SERVER_INFO)
-	{
-		output->AddByte(0x01);
-		if(g_game.getStagesEnabled())
-		{
-			output->AddByte(g_game.getStagesCount());
-			for(StageList::const_iterator it = g_game.getFirstStage(),
-				end = g_game.getLastStage(); it != end; ++it)
-			{
-				output->AddU32(it->first);
-				output->AddU32(it->second);
-			}
-		}
-		else
-			output->AddByte(0x00);
-	}
 
 	if(requestedInfo & REQUEST_EXT_PLAYERS_INFO)
 	{
