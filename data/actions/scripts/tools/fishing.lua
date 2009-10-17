4c9edfb8eb1eb5b8819d6e1502b6779a04bbfbed
Dawid Pietrzak <fightingelf@gmail.com>
2009-10-17 09:48:48 +0000

    * Improved fishing rod, fixed water decaying (was definitely too long!)
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@2988 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- data/actions/scripts/tools/fishing.lua
+++ data/actions/scripts/tools/fishing.lua
@@ -2,6 +2,8 @@ local config = {
 	waters = {4614, 4615, 4616, 4617, 4618, 4619, 4620, 4621, 4622, 4623, 4624, 4625, 4665, 4666, 4820, 4821, 4822, 4823, 4824, 4825},
 	fishable = {4608, 4609, 4610, 4611, 4612, 4613, 7236},
 	spawning = {4614, 4615, 4616, 4617, 4618, 4619},
+	holes = {7236},
+
 	corpses = {
 		-- [corpse] = {items}
 		[2025] = {
@@ -9,10 +11,15 @@ local config = {
 			-- TODO: Water elemental and Massive Water Elemental loot...
 		}
 	},
-	holes = {7236},
-
 	checkCorpseOwner = getConfigValue("checkCorpseOwner"),
 	rateLoot = getConfigValue("rateLoot"),
+
+	summons = {
+		-- {name, skill, chance}
+	},
+	rateSpawn = getConfigValue("rateSpawn"),
+
+	baitFailRemoveChance = 10,
 	allowFromPz = false,
 	useBait = true,
 	baitCount = 1,
@@ -23,7 +30,7 @@ config.checkCorpseOwner = getBooleanFromString(config.checkCorpseOwner)
 
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 	if(isInArray(config.waters, itemEx.itemid)) then
-		if(isInArray(config.spawnable, itemEx.itemid)) then
+		if(isInArray(config.spawning, itemEx.itemid)) then
 			doPlayerSendDefaultCancel(cid, RETURNVALUE_NOTPOSSIBLE)
 		end
 
@@ -61,34 +68,48 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 		return false
 	end
 
-	config.allowFromPz = config.allowFromPz or not getTileInfo(getThingPosition(cid)).protection
-	local formula, tries = getPlayerSkill(cid, SKILL_FISHING) / 200 + 0.85 * math.random(), 0
+	local position, formula, tries = getThingPosition(cid), getPlayerSkill(cid, SKILL_FISHING) / 200 + 0.85 * math.random(), 0
+	config.allowFromPz = config.allowFromPz or not getTileInfo(position).protection
 	if(item.itemid ~= ITEM_MECHANICAL_FISHING_ROD) then
-		if(config.allowFromPz and (not config.useBait or
-			(getPlayerItemCount(cid, ITEM_WORM) >= config.baitCount and
-			doPlayerRemoveItem(cid, ITEM_WORM, config.baitCount)))) then
+		if(config.allowFromPz and (not config.useBait or getPlayerItemCount(cid, ITEM_WORM) >= config.baitCount)) then
+			tries = 1
 			if(isInArray(config.holes, itemEx.itemid)) then
+				if(doPlayerRemoveItem(cid, ITEM_WORM, config.baitCount)) then
+					tries = 2
+					if(formula > 0.83) then
+						doPlayerAddItem(cid, ITEM_RAINBOW_TROUT, config.fishes)
+					elseif(formula > 0.7) then
+						doPlayerAddItem(cid, ITEM_NORTHERN_PIKE, config.fishes)
+					elseif(formula > 0.5) then
+						doPlayerAddItem(cid, ITEM_GREEN_PERCH, config.fishes)
+					else
+						doPlayerAddItem(cid, ITEM_FISH, config.fishes)
+					end
+				end
+			elseif(formula > 0.7 and doPlayerRemoveItem(cid, ITEM_WORM, config.baitCount)) then
 				tries = 2
-				if(formula > 0.83) then
-					doPlayerAddItem(cid, ITEM_RAINBOW_TROUT, config.fishes)
-				elseif(formula > 0.7) then
-					doPlayerAddItem(cid, ITEM_NORTHERN_PIKE, config.fishes)
-				elseif(formula > 0.5) then
-					doPlayerAddItem(cid, ITEM_GREEN_PERCH, config.fishes)
+				if(table.maxn(summons) > 0 and getDistanceBetween(position, toPosition) < 2) then
+					local skill, summon = getPlayerSkill(cid, SKILL_FISHING), {name = "", chance = 0}
+					for _, data in pairs(config.summons) do
+						if(skill >= data[2]) then
+							summon.name = data[1]
+							summon.chance = data[3]
+						end
+					end
+
+					if(summon.name ~= "" and summon.chance >= math.random(1, 100000) / config.rateSpawn) then
+						doCreateMonster(summon.name, position)
+						tries = 3
+					else
+						doPlayerAddItem(cid, ITEM_FISH, config.fishes)
+					end
 				else
 					doPlayerAddItem(cid, ITEM_FISH, config.fishes)
 				end
-			elseif(formula > 0.7) then
-				doPlayerAddItem(cid, ITEM_FISH, config.fishes)
-				tries = 2
-			else
-				tries = 1
 			end
 		end
-	elseif(config.allowFromPz and (not config.useBait or
-		(getPlayerItemCount(cid, ITEM_NAIL) >= config.baitCount and
-		doPlayerRemoveItem(cid, ITEM_NAIL, config.baitCount)))) then
-		if(formula > 0.7) then
+	elseif(config.allowFromPz and (not config.useBait or getPlayerItemCount(cid, ITEM_NAIL) >= config.baitCount)) then
+		if(formula > 0.7 and doPlayerRemoveItem(cid, ITEM_NAIL, config.baitCount)) then
 			doPlayerAddItem(cid, ITEM_MECHANICAL_FISH, config.fishes)
 			tries = 2
 		else
@@ -97,7 +118,7 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 	end
 
 	if(tries > 1) then
-		doPlayerAddSkillTry(cid, SKILL_FISHING, 2)
+		doPlayerAddSkillTry(cid, SKILL_FISHING, tries)
 		if(not isInArray(config.holes, itemEx.itemid)) then
 			doTransformItem(itemEx.uid, itemEx.itemid + 6)
 		else
@@ -107,6 +128,13 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 		doDecayItem(itemEx.uid)
 	elseif(tries > 0) then
 		doPlayerAddSkillTry(cid, SKILL_FISHING, 1)
+		if(config.baitFailRemoveChance >= math.random(1, 100)) then
+			if(item.itemid == ITEM_MECHANICAL_FISHING_ROD) then
+				doPlayerRemoveItem(cid, ITEM_NAIL, config.baitCount)
+			else
+				doPlayerRemoveItem(cid, ITEM_FISH, config.baitCount)
+			end
+		end
 	end
 
 	doSendMagicEffect(toPosition, CONST_ME_LOSEENERGY)
