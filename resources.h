7d3fb1fd561a68a0cbc7f8fb6e7d1c15234a8800
Dawid Pietrzak <fightingelf@gmail.com>
2009-11-14 13:21:09 +0000

    * 8.53 support ;p
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/branches/0.3@3105 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- resources.h
+++ resources.h
@@ -74,13 +74,13 @@
 #endif
 
 #define CLIENT_VERSION_MIN 850
-#define CLIENT_VERSION_MAX 852
+#define CLIENT_VERSION_MAX 853
 #define CLIENT_VERSION_STRING "Only clients with protocol 8.5x allowed!"
 
 #define STATUS_SERVER_NAME "The Forgotten Server"
 #define STATUS_SERVER_VERSION "0.3.6_SVN"
 #define STATUS_SERVER_CODENAME "Crying Damson"
-#define STATUS_SERVER_PROTOCOL "8.52"
+#define STATUS_SERVER_PROTOCOL "8.53"
 
 #define VERSION_CHECK "http://forgottenserver.otland.net/version.xml"
 #define VERSION_PATCH 0
