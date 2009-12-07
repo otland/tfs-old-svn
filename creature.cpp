9277a48094b84312bee9d48b47112100640f76f9
Dawid Pietrzak <fightingelf@gmail.com>
2009-12-07 09:53:11 +0000

    * Fixed the spam issue on branches
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/branches/0.2@3250 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- creature.cpp
+++ creature.cpp
@@ -1403,8 +1403,9 @@ bool Creature::hasCondition(ConditionType_t type, int32_t subId/* = 0*/, bool ch
 		if((*it)->getType() != type || (subId != -1 && (*it)->getSubId() != (uint32_t)subId))
 			continue;
 
-		return !checkTime || g_config.getBool(ConfigManager::OLD_CONDITION_ACCURACY)
-			|| !(*it)->getEndTime() || (*it)->getEndTime() >= OTSYS_TIME();
+		if(!checkTime || g_config.getBool(ConfigManager::OLD_CONDITION_ACCURACY)
+			|| !(*it)->getEndTime() || (*it)->getEndTime() >= OTSYS_TIME())
+			return true;
 	}
 
 	return false;
