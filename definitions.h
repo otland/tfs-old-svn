3cef5dd52f65e7676d439eca519c00e40b37eb65
Dawid Pietrzak <fightingelf@gmail.com>
2009-12-09 22:09:36 +0000

    * Basics of 8.54: we need new logon byte position, or whatever it is
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@3266 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- definitions.h
+++ definitions.h
@@ -148,14 +148,14 @@
 #endif
 #endif
 
-#define CLIENT_VERSION_MIN 857
-#define CLIENT_VERSION_MAX 857
-#define CLIENT_VERSION_STRING "Only clients with protocol 8.57 allowed!"
+#define CLIENT_VERSION_MIN 854
+#define CLIENT_VERSION_MAX 854
+#define CLIENT_VERSION_STRING "Only clients with protocol 8.54 allowed!"
 
 #define STATUS_SERVER_NAME "The Forgotten Server"
 #define STATUS_SERVER_VERSION "0.4_DEV"
 #define STATUS_SERVER_CODENAME ""
-#define STATUS_SERVER_PROTOCOL "8.57"
+#define STATUS_SERVER_PROTOCOL "8.54"
 
 #define VERSION_CHECK "http://forgottenserver.otland.net/version.xml"
 #define VERSION_PATCH 0
