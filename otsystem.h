fce7afee00225373f76146faf53ce78889bd23e6
thestian <thestian>
2009-12-13 04:11:36 +0000

    Fixed misspelled preprocessor function in otsystem.h, this will fix the build on Linux (tested with archlinux and ubuntu 9.10)
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/branches/0.3@3298 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- otsystem.h
+++ otsystem.h
@@ -76,7 +76,7 @@ inline int64_t OTSYS_TIME()
 
 #ifdef __GNUC__
 	#define __OTSERV_FUNCTION__ __PRETTY_FUNCTION__
-#elif def _MSC_VER
+#elif defined(_MSC_VER)
 	#define __OTSERV_FUNCTION__ __FUNCDNAME__
 #endif
 
