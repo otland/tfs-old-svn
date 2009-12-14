e9f84ecbae52afaac811b9345d151f1ad7dc9dd7
Dawid Pietrzak <fightingelf@gmail.com>
2009-12-14 10:54:50 +0000

    * Forgot to add pumpkinhead :P
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@3327 c161f52b-5830-4e7a-bee0-5dea2c17262e

---
+++ data/actions/scripts/other/pumpkinhead.lua
@@ -0,0 +1,21 @@
+local PUMPKIN = 2683
+local PUMPKINHEAD_LIGHT_OFF = 2096
+local PUMPKINHEAD_LIGHT_ON = 2097
+
+local CANDLE = 2048
+local KNIFE = 2566
+
+function onUse(cid, item, fromPosition, itemEx, toPosition)
+	if(item.itemid == PUMPKINHEAD_LIGHT_OFF and itemEx.itemid == CANDLE) then
+		doTransformItem(item.uid, PUMPKINHEAD_LIGHT_ON)
+		doRemoveItem(itemEx.uid)
+
+		doDecayItem(item.uid)
+		return true
+	elseif(item.itemid == KNIFE and itemEx.itemid == PUMPKIN) then
+		doTransformItem(itemEx.uid, PUMPKINHEAD_LIGHT_OFF)
+		return true
+	end
+
+	return false
+end
