d090a1a12f239fc45e5d3a8574937134921f2d44
Dawid Pietrzak <fightingelf@gmail.com>
2009-12-22 05:48:54 +0000

    * Fix to blueberry bush
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/branches/0.3@3424 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- data/actions/scripts/other/blueberrybush.lua
+++ data/actions/scripts/other/blueberrybush.lua
@@ -1,20 +1,7 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	doTransformItem(item.uid, ITEM_BUSH)
-	doCreateItem(ITEM_BLUEBERRY, 3, fromPosition)
-	addEvent(transformBack, 300000, fromPosition)
+	doCreateItem(2677, 3, fromPosition)
+	doTransformItem(item.uid, 2786)
+
+	doDecayItem(item.uid)
 	return true
-end
-
-function transformBack(position)
-	position.stackpos = STACKPOS_TOP_MOVEABLE_ITEM_OR_CREATURE
-
-	local topThing = getThingFromPos(position)
-	if(topThing.itemid == ITEM_BLUEBERRY) then
-		addEvent(transformBack, 300000, position)
-	else
-		topThing = getTileItemById(position, ITEM_BUSH)
-		if(topThing.itemid ~= 0) then
-			doTransformItem(topThing.uid, ITEM_BLUEBERRYBUSH)
-		end
-	end
 end
