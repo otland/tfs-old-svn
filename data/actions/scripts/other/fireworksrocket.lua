c7537a591084267dccc4e40a15c88635c57c370b
Dawid Pietrzak <fightingelf@gmail.com>
2010-01-03 21:20:21 +0000

    * Fix to fireworks rocket
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@3447 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- data/actions/scripts/other/fireworksrocket.lua
+++ data/actions/scripts/other/fireworksrocket.lua
@@ -1,12 +1,9 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 	if(fromPosition.x ~= CONTAINER_POSITION) then
-		fireworksEffect = math.random(CONST_ME_FIREWORK_YELLOW, CONST_ME_FIREWORK_BLUE)
-		doSendMagicEffect(fromPosition, fireworksEffect)
+		doSendMagicEffect(fromPosition, math.random(CONST_ME_FIREWORK_YELLOW, CONST_ME_FIREWORK_BLUE))
 	else
-		doSendMagicEffect(fromPosition, CONST_ME_HITBYFIRE)
-		doSendMagicEffect(fromPosition, CONST_ME_EXPLOSIONAREA)
-		doCreatureSay(cid, "Ouch! Rather place it on the ground next time.", TALKTYPE_ORANGE_1)
-		doCreatureAddHealth(cid, -10)
+		doCreatureSay(cid, "Ouch! Rather place it on the ground next time.", TALKTYPE_MONSTER)
+		doTargetCombatHealth(0, cid, COMBAT_PHYSICALDAMAGE, -1, -1, CONST_ME_EXPLOSIONHIT)
 	end
 
 	doRemoveItem(cid, item.uid, 1)
