88856e004bb7e1d0bbf1efe0d1b6ee5c2e5756ec
thestian <thestian>
2010-01-10 23:06:18 +0000

    * Fixed the dev-build script so it'll work like intended, also added an example when I used it, uses only 1/7 of the original compile time\!
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@3486 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- dev-build.sh
+++ dev-build.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
+# CCcache + multi core Compile script by Stian
+# For "The Forgotten Server"
+ 
+# Example:
+# # make clean
+# # time ./dev-build.sh
+
+# Gives:
+# real	3m27.070s
+# user	6m4.066s
+# sys	0m16.659s
+
+# CCACHE recompile (from stratch):
+# # make clean
+# # time ./dev-build.sh
+
+# Gives:
+# real	0m27.620s
+# user	0m43.744s
+# sys	0m4.766s
+
+# 1/7 of the original compile speed!
+# When more you do it, more ccache will cache, default is limited to use 1GB storage
+
 echo "TheForgottenServer test build script, seems to speed things up ALOT when testing new features. Requires ccache installed!"
 
 # Enable CCACHE
-export PATH=/usr/lib/ccache:$PATH
+export PATH=/usr/lib/ccache/bin:$PATH
 # Enable make processes - 1 + number of cores
  # Get cores
  cores=`grep cores /proc/cpuinfo | wc -l` 
  makeopt_j=$(($cores+1))
-echo "Building on $cores, using $makeopt_j processes"
+echo "Building on $cores cores, using $makeopt_j processes"
 make -j $makeopt_j
