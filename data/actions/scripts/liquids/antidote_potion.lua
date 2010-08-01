ac7874f3b4fd515fb8c79bb2ad9e2233881a423f
Dawid Pietrzak <fightingelf@gmail.com>
2010-08-01 04:06:56 +0000

    * seems it fixes the skull bug.
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@3769 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- data/actions/scripts/liquids/antidote_potion.lua
+++ data/actions/scripts/liquids/antidote_potion.lua
@@ -1,7 +1,6 @@
 local EMPTY_POTION = 7636
 
 local combat = createCombatObject()
-setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_HEALING)
 setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_BLUE)
 setCombatParam(combat, COMBAT_PARAM_TARGETCASTERORTOPMOST, true)
 setCombatParam(combat, COMBAT_PARAM_AGGRESSIVE, false)
