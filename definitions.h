116bd486e1722a32c2db1c1b3b48c5ab49a86c20
stian <stian>
2010-08-21 19:49:27 +0000

    This is not 0.3_SVN, it's 0.4
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@3819 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- definitions.h
+++ definitions.h
@@ -23,8 +23,8 @@
 #define CLIENT_VERSION_STRING "Only clients with protocol 8.6 allowed!"
 
 #define SOFTWARE_NAME "The Forgotten Server"
-#define SOFTWARE_VERSION "0.3_SVN"
-#define SOFTWARE_CODENAME "Crying Damson"
+#define SOFTWARE_VERSION "0.4_SVN"
+#define SOFTWARE_CODENAME ""
 #define SOFTWARE_PROTOCOL "8.6"
 
 #define VERSION_CHECK "http://forgottenserver.otland.net/version.xml"
