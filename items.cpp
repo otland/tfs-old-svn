0526887e68b6969e2a4e490c8f0a6e1c67f98478
slawkens <slawkens>
2010-10-16 19:34:33 +0000

    * bumped to 8.62 (using latest .otb from ot svn)
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@3937 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- items.cpp
+++ items.cpp
@@ -187,7 +187,7 @@ int32_t Items::loadFromOtb(std::string file)
 		std::clog << "[Error - Items::loadFromOtb] New version detected, an older version of items.otb is required." << std::endl;
 		return ERROR_INVALID_FORMAT;
 	}
-	else if(Items::dwMinorVersion != CLIENT_VERSION_861)
+	else if(Items::dwMinorVersion != CLIENT_VERSION_862)
 	{
 		std::clog << "[Error - Items::loadFromOtb] Another (client) version of items.otb is required." << std::endl;
 		return ERROR_INVALID_FORMAT;
