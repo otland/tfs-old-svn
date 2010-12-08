8371eb5053a92a9662658aaf48a276a849807286
Mark Samman <mark.samman@gmail.com>
2010-12-08 16:07:15 +0000

    * Updated potions script with 8.62 changes.
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/branches/0.2@3968 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- data/actions/scripts/other/potions.lua
+++ data/actions/scripts/other/potions.lua
@@ -39,28 +39,32 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 		end
 		doAddCondition(cid, exhaust)
 		doCreatureSay(cid, "Aaaah...", TALKTYPE_ORANGE_1)
-		doTransformItem(item.uid, emptyPot)
+		doRemoveItem(item.uid, 1)
+		doPlayerAddItem(cid, emptyPot, 1)
 	elseif(item.itemid == smallHealthPot) then
 		if(doTargetCombatHealth(0, cid, COMBAT_HEALING, 50, 100, CONST_ME_MAGIC_BLUE) == LUA_ERROR) then
 			return FALSE
 		end
 		doAddCondition(cid, exhaust)
 		doCreatureSay(cid, "Aaaah...", TALKTYPE_ORANGE_1)
-		doTransformItem(item.uid, emptyPot)
+		doRemoveItem(item.uid, 1)
+		doPlayerAddItem(cid, emptyPot, 1)
 	elseif(item.itemid == healthPot) then
 		if(doTargetCombatHealth(0, cid, COMBAT_HEALING, 100, 200, CONST_ME_MAGIC_BLUE) == LUA_ERROR) then
 			return FALSE
 		end
 		doAddCondition(cid, exhaust)
 		doCreatureSay(cid, "Aaaah...", TALKTYPE_ORANGE_1)
-		doTransformItem(item.uid, emptyPot)
+		doRemoveItem(item.uid, 1)
+		doPlayerAddItem(cid, emptyPot, 1)
 	elseif(item.itemid == manaPot) then
 		if(doTargetCombatMana(0, cid, 70, 130, CONST_ME_MAGIC_BLUE) == LUA_ERROR) then
 			return FALSE
 		end
 		doAddCondition(cid, exhaust)
 		doCreatureSay(cid, "Aaaah...", TALKTYPE_ORANGE_1)
-		doTransformItem(item.uid, emptyPot)
+		doRemoveItem(item.uid, 1)
+		doPlayerAddItem(cid, emptyPot, 1)
 	elseif(item.itemid == strongHealthPot) then
 		if(not(isKnight(cid) or isPaladin(cid)) or (getPlayerLevel(cid) < 50)) and not(getPlayerGroupId(cid) >= 2) then
 			doCreatureSay(cid, "This potion can only be consumed by paladins and knights of level 50 or higher.", TALKTYPE_ORANGE_1)
@@ -72,7 +76,8 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 		end
 		doAddCondition(cid, exhaust)
 		doCreatureSay(cid, "Aaaah...", TALKTYPE_ORANGE_1)
-		doTransformItem(item.uid, strongEmptyPot)
+		doRemoveItem(item.uid, 1)
+		doPlayerAddItem(cid, strongEmptyPot, 1)
 	elseif(item.itemid == strongManaPot) then
 		if(not(isSorcerer(cid) or isDruid(cid) or isPaladin(cid)) or (getPlayerLevel(cid) < 50)) and not(getPlayerGroupId(cid) >= 2) then
 			doCreatureSay(cid, "This potion can only be consumed by sorcerers, druids and paladins of level 50 or higher.", TALKTYPE_ORANGE_1)
@@ -84,7 +89,8 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 		end
 		doAddCondition(cid, exhaust)
 		doCreatureSay(cid, "Aaaah...", TALKTYPE_ORANGE_1)
-		doTransformItem(item.uid, strongEmptyPot)
+		doRemoveItem(item.uid, 1)
+		doPlayerAddItem(cid, strongEmptyPot, 1)
 	elseif(item.itemid == greatSpiritPot) then
 		if(not(isPaladin(cid)) or (getPlayerLevel(cid) < 80)) and not(getPlayerGroupId(cid) >= 2) then
 			doCreatureSay(cid, "This potion can only be consumed by paladins of level 80 or higher.", TALKTYPE_ORANGE_1)
@@ -96,7 +102,8 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 		end
 		doAddCondition(cid, exhaust)
 		doCreatureSay(cid, "Aaaah...", TALKTYPE_ORANGE_1)
-		doTransformItem(item.uid, greatEmptyPot)
+		doRemoveItem(item.uid, 1)
+		doPlayerAddItem(cid, greatEmptyPot, 1)
 	elseif(item.itemid == greatHealthPot) then
 		if(not(isKnight(cid)) or (getPlayerLevel(cid) < 80)) and not(getPlayerGroupId(cid) >= 2) then
 			doCreatureSay(cid, "This potion can only be consumed by knights of level 80 or higher.", TALKTYPE_ORANGE_1)
@@ -108,7 +115,8 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 		end
 		doAddCondition(cid, exhaust)
 		doCreatureSay(cid, "Aaaah...", TALKTYPE_ORANGE_1)
-		doTransformItem(item.uid, greatEmptyPot)
+		doRemoveItem(item.uid, 1)
+		doPlayerAddItem(cid, greatEmptyPot, 1)
 	elseif(item.itemid == greatManaPot) then
 		if(not(isSorcerer(cid) or isDruid(cid)) or (getPlayerLevel(cid) < 80)) and not(getPlayerGroupId(cid) >= 2) then
 			doCreatureSay(cid, "This potion can only be consumed by sorcerers and druids of level 80 or higher.", TALKTYPE_ORANGE_1)
@@ -120,7 +128,8 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 		end
 		doAddCondition(cid, exhaust)
 		doCreatureSay(cid, "Aaaah...", TALKTYPE_ORANGE_1)
-		doTransformItem(item.uid, greatEmptyPot)
+		doRemoveItem(item.uid, 1)
+		doPlayerAddItem(cid, greatEmptyPot, 1)
 	elseif(item.itemid == ultimateHealthPot) then
 		if(not(isKnight(cid)) or (getPlayerLevel(cid) < 130)) and not(getPlayerGroupId(cid) >= 2) then
 			doCreatureSay(cid, "This potion can only be consumed by knights of level 130 or higher.", TALKTYPE_ORANGE_1)
@@ -132,7 +141,8 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 		end
 		doAddCondition(cid, exhaust)
 		doCreatureSay(cid, "Aaaah...", TALKTYPE_ORANGE_1)
-		doTransformItem(item.uid, greatEmptyPot)
+		doRemoveItem(item.uid, 1)
+		doPlayerAddItem(cid, greatEmptyPot, 1)
 	end
 	return TRUE
 end
