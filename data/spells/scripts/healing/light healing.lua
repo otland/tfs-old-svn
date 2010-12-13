4dc148f81f62a6d5d78a8e3e4f9d178d3e746787
Mark Samman <mark.samman@gmail.com>
2010-12-13 08:24:42 +0000

    * Updated changelog. * Reverted changes made to light healing forumla in Fallen's commit.
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/branches/0.2@3988 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- data/spells/scripts/healing/light healing.lua
+++ data/spells/scripts/healing/light healing.lua
@@ -3,7 +3,7 @@ setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_HEALING)
 setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_BLUE)
 setCombatParam(combat, COMBAT_PARAM_AGGRESSIVE, false)
 setCombatParam(combat, COMBAT_PARAM_DISPEL, CONDITION_PARALYZE)
-setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, 5, 5, 1.5, 2)
+setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, 0.08, 0, 0.33, 0)
 
 function onCastSpell(cid, var)
 	return doCombat(cid, combat, var)
