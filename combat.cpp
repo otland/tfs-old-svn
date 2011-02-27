3f70f1f0cfa050a4e94d4124a3838e2f9bb26e74
Dawid Pietrzak <fightingelf@gmail.com>
2011-02-27 19:47:21 +0000

    http://tibia.wikia.com/wiki/Stealth_Ring
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@4225 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- combat.cpp
+++ combat.cpp
@@ -601,6 +601,16 @@ bool Combat::CombatDispelFunc(Creature* caster, Creature* target, const CombatPa
 	if(!target->hasCondition(params.dispelType))
 		return false;
 
+	Player* player = target->getPlayer();
+	if(player && (g_game.getWorldType() == WORLDTYPE_HARDCORE
+		|| getTile()->hasFlag(TILESTATE_HARDCOREZONE)) &&
+		random_range(0, 100) <= 10) // CHECKME: needs confirmation
+	{
+		Item* player->getEquippedItem(SLOT_RING);
+		if(item && item->getID() == ITEM_STEALTH_RING)
+			g_game.internalRemoveItem(NULL, item);
+	}	
+
 	target->removeCondition(caster, params.dispelType);
 	return true;
 }
