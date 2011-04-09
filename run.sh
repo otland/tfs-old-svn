622c34d63caa3914922139b990fa50ede1a3dae5
fallen <fallen>
2011-04-09 16:06:13 +0000

    * Added run script
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@4359 c161f52b-5830-4e7a-bee0-5dea2c17262e

---
+++ run.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+#The forgotten server run script
+
+echo "The forgotten server run script"
+VERSION="1.00a"
+
+case "$1" in
+  --help|-h)
+      echo >&2 "Usage $0 <options>"
+      echo >&2 " Where options are:"
+      echo >&2 " -h|--help|start|stop|restart|kill|--version";
+      if [ -f theforgottenserver ];
+      then
+        ./theforgottenserver --help
+      fi
+      ;;
+   start|s)
+     if [ -f theforgottenserver ];
+     then
+        echo >&2 "Running file transfer server v$VERSION"
+        ./theforgottenserver
+        echo >&2 "Done"
+      else
+         echo >&2 "Could not start file transfer server"
+      fi
+     ;;
+   restart)
+     echo "Restarting..."
+     killall -9 theforgottenserver
+     if [ -f theforgottenserver ];
+     then
+        ./theforgottenserver
+     else
+        echo >&2 "Failed"
+     fi
+     ;;
+   stop|kill)
+     killall -9 theforgottenserver
+     ;;
+   --version)
+     echo "v$VERSION"
+     exit
+     ;;
+   *)
+      echo >&2 "Usage $0 <options>"
+      echo >&2 " Where options are:"
+      echo >&2 " -h|--help|start|stop|restart|kill";
+      ;;
+esac
