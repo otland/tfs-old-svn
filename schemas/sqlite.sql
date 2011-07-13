5cdd4e0b5c8e501dcc96d11f1bbaa7bec82de3a4
Mark Samman <mark.samman@gmail.com>
2011-07-13 23:43:17 +0000

    * Fixed a error in the sqlite schema. * Recreated the sqlite database.
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@4468 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- schemas/sqlite.sql
+++ schemas/sqlite.sql
@@ -111,7 +111,7 @@ CREATE TABLE "players" (
 	FOREIGN KEY ("account_id") REFERENCES "accounts" ("id")
 );
 
-INSERT INTO "players" VALUES (1, 'Account Manager', 0, 1, 1, 1, 0, 150, 150, 0, 0, 0, 0, 0, 110, 0, 0, 0, 0, 0, 0, 0, 0, 853, 921, 7, '', 400, 0, 0, 0, 0, 0, 0, 0, '', 0, 0, 0, 201660000, 0, 100, 100, 100, 100, 100, 0, 0, 0, 0, 0, '');
+INSERT INTO "players" VALUES (1, 'Account Manager', 0, 1, 1, 1, 0, 150, 150, 0, 0, 0, 0, 0, 110, 0, 0, 0, 0, 0, 0, 0, 0, 853, 921, 7, '', 400, 0, 0, 0, 0, 0, 0, 0, '', 0, 0, 0, 0, 201660000, 0, 100, 100, 100, 100, 100, 0, 0, 0, 0, 0, '');
 
 CREATE TABLE "account_viplist" (
 	"account_id" INTEGER NOT NULL,
