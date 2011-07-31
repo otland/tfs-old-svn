35e662bcfea19b65718e0b0ed9f070d3e004c41a
benis <benis>
2011-07-31 19:49:03 +0000

    *Fixed/Updated movements (Also added name tags) *Fixed/Updated weapons (Also added name tags) *Added junglemaw moveevent *Fixed some issues with spells *Fixed small typo (Name changed :P teh maverick here)
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@4546 c161f52b-5830-4e7a-bee0-5dea2c17262e

---
+++ data/movements/scripts/junglemaw.lua
@@ -0,0 +1,11 @@
+function onStepIn(cid, item, pos)
+	doTargetCombatHealth(0, cid, COMBAT_POISONDAMAGE, -30, -30, CONST_ME_POFF)
+	doTransformItem(item.uid, item.itemid + 1)
+	return TRUE
+end
+
+function onAddItem(moveitem, tileitem, position)
+	doTransformItem(tileitem.uid, tileitem.itemid + 1)
+	doSendMagicEffect(position, CONST_ME_POFF)
+	return TRUE
+end
