87fd60964c325c979524cda8e365c5203ab3bd44
Mark Samman <mark.samman@gmail.com>
2011-09-02 01:51:23 +0000

    * Commented out code that caused crash (see: http://otland.net/project.php?issueid=1935).
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@4620 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- player.cpp
+++ player.cpp
@@ -2782,11 +2782,13 @@ ReturnValue Player::__queryAdd(int32_t index, const Thing* thing, uint32_t count
 		if(ret == RET_NOERROR && item->getWeaponType() != WEAPON_NONE)
 			const_cast<Player*>(this)->setLastAttack(OTSYS_TIME());
 
+		/*
 		if(ret == RET_BOTHHANDSNEEDTOBEFREE)
 		{
 			if(g_game.internalAddItem(NULL, const_cast<Player*>(this), inventory[(slots_t)index]) == RET_NOERROR)
 				g_game.internalRemoveItem(NULL, inventory[(slots_t)index]);
 		}
+		*/
 	}
 
 	return ret;
