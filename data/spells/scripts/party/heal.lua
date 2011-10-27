cf1427881cb101613dd32660197314367a0591b1
Dawid Pietrzak <fightingelf@gmail.com>
2011-10-27 21:32:28 +0000

    * party heal wouldn't stack with utura/utura gran
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@4784 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- data/spells/scripts/party/heal.lua
+++ data/spells/scripts/party/heal.lua
@@ -5,7 +5,7 @@ setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_BLUE)
 setCombatParam(combat, COMBAT_PARAM_AGGRESSIVE, false)
 
 local condition = createConditionObject(CONDITION_REGENERATION)
-setConditionParam(condition, CONDITION_PARAM_SUBID, 1)
+setConditionParam(condition, CONDITION_PARAM_SUBID, 2)
 setConditionParam(condition, CONDITION_PARAM_BUFF, true)
 setConditionParam(condition, CONDITION_PARAM_TICKS, 2 * 60 * 1000)
 setConditionParam(condition, CONDITION_PARAM_HEALTHGAIN, 20)
