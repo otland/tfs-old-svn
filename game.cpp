db1541eb803913c9f52e6d09b3a63292ce656a2e
Dawid Pietrzak <fightingelf@gmail.com>
2011-11-01 20:11:54 +0000

    im bit drunk
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@4814 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- game.cpp
+++ game.cpp
@@ -6180,7 +6180,7 @@ void Game::cleanup()
 
 void Game::freeThing(Thing* thing)
 {
-	if(std::find(releaseThing.begin(), releaseThing.end(), thing) == releaseThings.end())
+	if(std::find(releaseThings.begin(), releaseThings.end(), thing) == releaseThings.end())
 		releaseThings.push_back(thing);
 }
 
