b4ab25148076885ab0286ebfea2e94f279930f40
Dawid Pietrzak <fightingelf@gmail.com>
2011-11-09 22:25:00 +0000

    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@4856 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- data/actions/scripts/other/doors.lua
+++ data/actions/scripts/other/doors.lua
@@ -10,15 +10,15 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 		return true
 	end
 
-	local locked = DOORS[item.itemid]
+	local locked = DOORS[item.id]
 	if(locked) then
 		doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "It is locked.")
 		return true
 	end
 
-	local door = getItemInfo(item.itemid)
+	local door = getItemInfo(item.id)
 	if(door.levelDoor > 0) then
-		if(item.actionid == 189) then
+		if(item.aid == 189) then
 			if(not isPremium(cid)) then
 				doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "Only the worthy may pass.")
 				return true
@@ -27,7 +27,7 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 			return doorEnter(cid, item.uid, door.transformUseTo, toPosition)
 		end
 
-		local gender = item.actionid - 186
+		local gender = item.aid - 186
 		if(isInArray({PLAYERSEX_FEMALE,  PLAYERSEX_MALE}, gender)) then
 			if(gender ~= getPlayerSex(cid)) then
 				doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "Only the worthy may pass.")
@@ -37,7 +37,7 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 			return doorEnter(cid, item.uid, door.transformUseTo, toPosition)
 		end
 
-		local skull = item.actionid - 180
+		local skull = item.aid - 180
 		if(skull >= SKULL_NONE and skull <= SKULL_BLACK) then
 			if(skull ~= getCreatureSkullType(cid)) then
 				doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "Only the worthy may pass.")
@@ -47,7 +47,7 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 			return doorEnter(cid, item.uid, door.transformUseTo, toPosition)
 		end
 
-		local group = item.actionid - 150
+		local group = item.aid - 150
 		if(group >= 0 and group < 30) then
 			if(group > getPlayerGroupId(cid)) then
 				doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "Only the worthy may pass.")
@@ -57,7 +57,7 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 			return doorEnter(cid, item.uid, door.transformUseTo, toPosition)
 		end
 
-		local vocation = item.actionid - 100
+		local vocation = item.aid - 100
 		if(vocation >= 0 and vocation < 50) then
 			local vocationEx = getVocationInfo(getPlayerVocation(cid))
 			if(vocationEx.id ~= vocation and vocationEx.fromVocation ~= vocation) then
@@ -68,7 +68,7 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 			return doorEnter(cid, item.uid, door.transformUseTo, toPosition)
 		end
 
-		if(item.actionid == 190 or (item.actionid ~= 0 and getPlayerLevel(cid) >= (item.actionid - doors.levelDoor))) then
+		if(item.aid == 190 or (item.aid ~= 0 and getPlayerLevel(cid) >= (item.aid - doors.levelDoor))) then
 			return doorEnter(cid, item.uid, door.transformUseTo, toPosition)
 		end
 
@@ -77,7 +77,7 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 	end
 
 	if(door.specialDoor) then
-		if(item.actionid == 100 or (item.actionid ~= 0 and getCreatureStorage(cid, item.actionid) > 0)) then
+		if(item.aid == 100 or (item.aid ~= 0 and getCreatureStorage(cid, item.aid) > 0)) then
 			return doorEnter(cid, item.uid, door.transformUseTo, toPosition)
 		end
 
@@ -87,12 +87,12 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 
 	toPosition.stackpos = STACKPOS_TOP_MOVEABLE_ITEM_OR_CREATURE
 	local fields, thing = getTileItemsByType(fromPosition, ITEM_TYPE_MAGICFIELD), getThingFromPosition(toPosition)
-	if(item.uid == thing.uid or thing.itemid < 100 or table.maxn(fields) ~= 0) then
+	if(item.uid == thing.uid or thing.id < 100 or table.maxn(fields) ~= 0) then
 		return true
 	end
 
 	local doorCreature = getThingFromPosition(toPosition)
-	if(doorCreature.itemid ~= 0) then
+	if(doorCreature.id ~= 0) then
 		toPosition.x = toPosition.x + 1
 		local query = doTileQueryAdd(doorCreature, toPosition, 20) -- allow to stack outside doors, but not on teleports or floor changing tiles
 		if(query == RETURNVALUE_NOTPOSSIBLE) then
