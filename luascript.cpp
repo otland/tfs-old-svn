f299bd73814bfa976e820d63e26e3813b711dee7
Dawid Pietrzak <fightingelf@gmail.com>
2011-11-12 08:03:50 +0000

    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@4895 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- luascript.cpp
+++ luascript.cpp
@@ -5034,7 +5034,10 @@ int32_t LuaInterface::luaGetTileInfo(lua_State* L)
 	if(Tile* tile = g_game.getMap()->getTile(pos))
 	{
 		ScriptEnviroment* env = getEnv();
-		pushThing(L, tile->ground, env->addThing(tile->ground));
+		if(tile->ground && !tile->ground->isRemoved())
+			pushThing(L, tile->ground, env->addThing(tile->ground));
+		else
+			pushThing(L, NULL, 0);
 
 		setFieldBool(L, "protection", tile->hasFlag(TILESTATE_PROTECTIONZONE));
 		setFieldBool(L, "optional", tile->hasFlag(TILESTATE_OPTIONALZONE));
