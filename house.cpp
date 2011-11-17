41b2506113bbca7c1d62cfbd75e02461d23771d5
Dawid Pietrzak <fightingelf@gmail.com>
2011-11-17 18:17:57 +0000

    * Fixed a huge bug with players not receiving items to depot if they have passed the house warnings limit.
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@4976 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- house.cpp
+++ house.cpp
@@ -939,10 +939,7 @@ bool Houses::payHouse(House* house, time_t _time, uint32_t bid)
 	{
 		house->setOwnerEx(0, true);
 		if(player->isVirtual())
-		{
-			IOLoginData::getInstance()->savePlayer(player);
 			delete player;
-		}
 			 
 		return false;
 	}
