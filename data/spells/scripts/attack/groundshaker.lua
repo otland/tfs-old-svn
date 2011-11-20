b9afd490c267bce7c48adde02eec9ff5caf8599d
Dawid Pietrzak <fightingelf@gmail.com>
2011-11-20 02:15:43 +0000

    * now groundshaker works properly :p
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@5006 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- data/spells/scripts/attack/groundshaker.lua
+++ data/spells/scripts/attack/groundshaker.lua
@@ -7,27 +7,31 @@ local area = createCombatArea(AREA_CIRCLE3X3)
 setCombatArea(combat, area)
 
 local WEAPON_SKILL = { SKILL_SWORD, SKILL_CLUB, SKILL_AXE, SKILL_DISTANCE, SKILL_SHIELD, SKILL_FIST }
-function onGetFormulaValues(cid, level, skill, attack, factor)
+function formula(level, skill, attack)
 	local skillTotal, levelTotal = skill + attack, level / 5
 	return -(skillTotal * 0.5 + levelTotal), -(skillTotal * 1.1 + levelTotal)
+end
+
+function onGetFormulaValues(cid, level, skill, attack, factor)
+	return formula(level, skill, attack)
 end
 
 setCombatCallback(combat, CALLBACK_PARAM_SKILLVALUE, "onGetFormulaValues")
 function onCastSpell(cid, var)
 	local weapon = getPlayerWeapon(cid)
 	if(weapon.uid == 0) then
-		return doCombat(cid, combat, var)
+		return false
 	end
 
 	local info = getItemInfo(weapon.itemid)
-	if(info.elementType == COMBAT_NONE) then
+	if(info.abilities.elementType == COMBAT_NONE) then
 		return doCombat(cid, combat, var)
 	end
 
 	local result, values = doCombat(cid, combat, var), {
-		onGetFormulaValues(cid, getPlayerLevel(cid), getPlayerSkillLevel(cid, WEAPON_SKILL[info.weaponType + 1]), info.elementDamage)
+		formula(getPlayerLevel(cid), getPlayerSkillLevel(cid, WEAPON_SKILL[info.weaponType + 1]), info.abilities.elementDamage)
 	}
 
-	doCombatAreaHealth(cid, info.elementType, getThingPosition(cid), area, values[1], values[2], CONST_ME_NONE)
+	doCombatAreaHealth(cid, info.abilities.elementType, getThingPosition(cid), area, values[1], values[2], CONST_ME_NONE)
 	return result
 end
