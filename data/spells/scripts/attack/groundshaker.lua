b18b912313e3aa387a3519ac21056ce4c990b093
Dawid Pietrzak <fightingelf@gmail.com>
2011-11-20 01:28:31 +0000

    tiny fix
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@5005 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- data/spells/scripts/attack/groundshaker.lua
+++ data/spells/scripts/attack/groundshaker.lua
@@ -6,7 +6,7 @@ setCombatParam(combat, COMBAT_PARAM_USECHARGES, true)
 local area = createCombatArea(AREA_CIRCLE3X3)
 setCombatArea(combat, area)
 
-local SKILL = { SKILL_SWORD, SKILL_CLUB, SKILL_AXE, SKILL_DISTANCE, SKILL_SHIELD, SKILL, SKILL_FIST }
+local WEAPON_SKILL = { SKILL_SWORD, SKILL_CLUB, SKILL_AXE, SKILL_DISTANCE, SKILL_SHIELD, SKILL_FIST }
 function onGetFormulaValues(cid, level, skill, attack, factor)
 	local skillTotal, levelTotal = skill + attack, level / 5
 	return -(skillTotal * 0.5 + levelTotal), -(skillTotal * 1.1 + levelTotal)
@@ -25,7 +25,7 @@ function onCastSpell(cid, var)
 	end
 
 	local result, values = doCombat(cid, combat, var), {
-		onGetFormulaValues(cid, getPlayerLevel(cid), getPlayerSkillLevel(cid, SKILL[info.weaponType]), info.elementDamage)
+		onGetFormulaValues(cid, getPlayerLevel(cid), getPlayerSkillLevel(cid, WEAPON_SKILL[info.weaponType + 1]), info.elementDamage)
 	}
 
 	doCombatAreaHealth(cid, info.elementType, getThingPosition(cid), area, values[1], values[2], CONST_ME_NONE)
