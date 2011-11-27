d36f33dac3f85e0f2a3d28c4d48d675ce91f45a5
Dawid Pietrzak <fightingelf@gmail.com>
2011-11-27 23:38:50 +0000

    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@5064 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- luascript.cpp
+++ luascript.cpp
@@ -756,10 +756,10 @@ bool LuaInterface::loadFile(const std::string& file, Npc* npc/* = NULL*/)
 	return true;
 }
 
-bool LuaInterface::loadDirectory(const std::string& dir, bool recursively, bool loadSystems, Npc* npc/* = NULL*/)
+bool LuaInterface::loadDirectory(std::string dir, bool recursively, bool loadSystems, Npc* npc/* = NULL*/)
 {
 	if(dir[dir.size() - 1] != '/')
-		dir += "/";
+		dir += '/';
 
 	StringVec files;
 	for(boost::filesystem::directory_iterator it(dir), end; it != end; ++it)
