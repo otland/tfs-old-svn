b44b26971d50b682429ae66f38a9e3e56bcccb9e
Dawid Pietrzak <fightingelf@gmail.com>
2012-01-16 07:11:43 +0000

    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@5243 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- items.cpp
+++ items.cpp
@@ -188,7 +188,7 @@ int32_t Items::loadFromOtb(std::string file)
 		std::clog << "[Error - Items::loadFromOtb] New version detected, an older version of items.otb is required." << std::endl;
 		return ERROR_INVALID_FORMAT;
 	}
-	else if(!g_config.getBool(ConfigManager::SKIP_ITEMS_VERSION) && Items::dwMinorVersion != CLIENT_VERSION_920)
+	else if(!g_config.getBool(ConfigManager::SKIP_ITEMS_VERSION) && Items::dwMinorVersion != CLIENT_VERSION_944)
 	{
 		std::clog << "[Error - Items::loadFromOtb] Another (client) version of items.otb is required." << std::endl;
 		return ERROR_INVALID_FORMAT;
