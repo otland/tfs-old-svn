d6d387088044947e87a21724b223fea7073962c1
Mark Samman <mark.samman@gmail.com>
2012-03-10 17:31:50 +0000

    * Updated Dev-Cpp and MSVC project files.
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/branches/0.2@5446 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- dev-cpp/TheForgottenServer-console-debug_private.h
+++ dev-cpp/TheForgottenServer-console-debug_private.h
@@ -5,19 +5,19 @@
 #define THEFORGOTTENSERVER_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"0.2.0.0"
+#define VER_STRING	"0.2.12.0"
 #define VER_MAJOR	0
 #define VER_MINOR	2
-#define VER_RELEASE	0
+#define VER_RELEASE	12
 #define VER_BUILD	0
-#define COMPANY_NAME	""
-#define FILE_VERSION	"0.2.0"
+#define COMPANY_NAME	"http://otland.net"
+#define FILE_VERSION	"0.2.12"
 #define FILE_DESCRIPTION	"The Forgotten Server"
 #define INTERNAL_NAME	"Mystic Spirit"
 #define LEGAL_COPYRIGHT	""
 #define LEGAL_TRADEMARKS	""
 #define ORIGINAL_FILENAME	"TheForgottenServer.exe"
 #define PRODUCT_NAME	"The Forgotten Server"
-#define PRODUCT_VERSION	"0.2.0"
+#define PRODUCT_VERSION	"0.2.12"
 
 #endif /*THEFORGOTTENSERVER_PRIVATE_H*/
--- dev-cpp/TheForgottenServer-debug_private.h
+++ dev-cpp/TheForgottenServer-debug_private.h
@@ -5,19 +5,19 @@
 #define THEFORGOTTENSERVER-DEBUG_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"0.2.7.0"
+#define VER_STRING	"0.2.12.0"
 #define VER_MAJOR	0
 #define VER_MINOR	2
-#define VER_RELEASE	7
+#define VER_RELEASE	12
 #define VER_BUILD	0
-#define COMPANY_NAME	""
-#define FILE_VERSION	"0.2.7-debug"
+#define COMPANY_NAME	"http://otland.net"
+#define FILE_VERSION	"0.2.12-debug"
 #define FILE_DESCRIPTION	"The Forgotten Server"
-#define INTERNAL_NAME	""
+#define INTERNAL_NAME	"Mystic Spirit"
 #define LEGAL_COPYRIGHT	""
 #define LEGAL_TRADEMARKS	""
 #define ORIGINAL_FILENAME	"TheForgottenServer.exe"
 #define PRODUCT_NAME	"The Forgotten Server"
-#define PRODUCT_VERSION	"0.2.7-debug"
+#define PRODUCT_VERSION	"0.2.12-debug"
 
 #endif /*THEFORGOTTENSERVER-DEBUG_PRIVATE_H*/
