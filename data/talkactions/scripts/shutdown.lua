05c2143d197a5f65554fab309264df2ace32f0f3
benis <benis>
2012-03-10 02:09:00 +0000

    * Shutdown talkaction now has a new 'reason' param: /shutdown 10, Fixing some bugs! Also will prompt every 5 minutes until last 5 minutes.
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@5441 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- data/talkactions/scripts/shutdown.lua
+++ data/talkactions/scripts/shutdown.lua
@@ -6,17 +6,30 @@ function onSay(cid, words, param, channel)
 		return true
 	end
 
-	if(isInArray({"cancel", "stop"}, param:lower())) then
-		stopEvent(shutdownEvent)
-		shutdownEvent = 0
-		return true
-	elseif(param:lower() == "kill") then
-		os.exit()
-		return true
+	params = string.explode(param, ",")
+	local action, reason, mins  = "", "", 0
+	if(not tonumber(params[1])) then
+		action = string.trim(params[1]:lower())
+	else
+		mins = tonumber(string.trim(params[1]))
+		if(table.maxn(params) > 1) then
+			reason = params[2]
+		end
+	end
+	
+	if(action) then
+		if(action == "cancel" or action == "stop") then
+			stopEvent(shutdownEvent)
+			shutdownEvent = 0
+			doPlayerSendCancel(cid, "Shutdown cancelled.")
+			return true
+		elseif(action == "kill") then
+			os.exit()
+			return true
+		end
 	end
 
-	param = tonumber(param)
-	if(not param or param < 0) then
+	if(not mins or mins < 0) then
 		doPlayerSendCancel(cid, "Numeric param may not be lower than 0.")
 		return true
 	end
@@ -25,23 +38,25 @@ function onSay(cid, words, param, channel)
 		stopEvent(shutdownEvent)
 	end
 
-	return prepareShutdown(math.abs(math.ceil(param)))
+	return prepareShutdown(math.abs(math.ceil(mins)), reason)
 end
 
-function prepareShutdown(minutes)
+function prepareShutdown(minutes, reason)
 	if(minutes <= 0) then
 		doSetGameState(GAMESTATE_SHUTDOWN)
 		return false
 	end
 
+	local change, r = 5, (reason and " Reason: "..reason or "")
 	if(minutes == 1) then
-		doBroadcastMessage("Server is going down in " .. minutes .. " minute, please log out now!")
-	elseif(minutes <= 3) then
-		doBroadcastMessage("Server is going down in " .. minutes .. " minutes, please log out.")
+		doBroadcastMessage("Server is going down in " .. minutes .. " minute, please log out now! Will be back online soon! " .. r)
+	elseif(minutes <= 5) then
+		doBroadcastMessage("Server is going down in " .. minutes .. " minutes, please log out. Will be back online soon!" .. r)
+		change = 1
 	else
-		doBroadcastMessage("Server is going down in " .. minutes .. " minutes.")
+		doBroadcastMessage("Server is going down in " .. minutes .. " minutes. Will be back online soon!" .. r)
 	end
 
-	shutdownEvent = addEvent(prepareShutdown, 60000, minutes - 1)
+	shutdownEvent = addEvent(prepareShutdown, (change * 60 * 1000), minutes - change, reason)
 	return true
 end
