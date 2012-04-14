ac9bb7ffdc0eeca3b907bb0499f8eebd26e1d001
Mark Samman <mark.samman@gmail.com>
2012-04-14 22:45:54 +0000

    * 9.52 otb (changes: http://pastebin.com/raw.php?i=QmBimVfe)
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/branches/0.2@5624 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- resources.h
+++ resources.h
@@ -44,6 +44,6 @@
 
 #define STATUS_SERVER_NAME "The Forgotten Server"
 #define STATUS_SERVER_CODENAME "Mystic Spirit"
-#define STATUS_SERVER_PROTOCOL "9.51"
+#define STATUS_SERVER_PROTOCOL "9.52"
 #define STATUS_SERVER_VERSION "0.2.13"
 #define STATUS_SERVER_DEVELOPERS "Talaturen, Kornholijo, Elf, Tryller, and Dalkon"
