125b77d3eff1855bcdcbc4d1b4b357c7c9221151
tryller <tryller>
2012-04-16 18:29:34 +0000

    * Fixes
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/trunk@5635 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- protocolgame.cpp
+++ protocolgame.cpp
@@ -2258,7 +2258,7 @@ void ProtocolGame::sendMarketDetail(uint16_t itemId)
 		ss.str("");
 		ss << it.defense;
 		if(it.extraDefense != 0)
-			ss << " " << it.extraDefense;
+			ss << " " << std::showpos << it.extraDefense << std::noshowpos;
 
 		msg->putString(ss.str());
 	}
@@ -2289,7 +2289,7 @@ void ProtocolGame::sendMarketDetail(uint16_t itemId)
 	if(it.hasAbilities())
 	{
 		bool separator = false;
-		for(uint32_t i = (COMBAT_FIRST + 1); i <= COMBAT_LAST; i <<= 1)
+		for(uint32_t i = (COMBAT_FIRST + 1); i <= COMBAT_LAST; ++i)
 		{
 			if(!it.abilities->absorb[i])
 				continue;
@@ -2402,7 +2402,8 @@ void ProtocolGame::sendMarketDetail(uint16_t itemId)
 	else
 		msg->put<char>(0x00);
 	
-	if((statistics = IOMarket::getInstance()->getSaleStatistics(itemId)))
+	statistics = IOMarket::getInstance()->getSaleStatistics(itemId);
+	if(statistics)
 	{
 		msg->put<char>(0x01);
 		msg->put<uint32_t>(statistics->numTransactions);
