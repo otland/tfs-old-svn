ea7ce9a619e3f1e7c980cb7c1d15820e7dd08fad
Mark Samman <mark.samman@gmail.com>
2013-02-28 01:44:02 +0000

    * Game protocol 9.82 support.
    git-svn-id: file:///usr/local/src/otland/tfssvnrepo/branches/0.2@5932 c161f52b-5830-4e7a-bee0-5dea2c17262e

--- resources.h
+++ resources.h
@@ -43,6 +43,6 @@
 
 #define STATUS_SERVER_NAME "The Forgotten Server"
 #define STATUS_SERVER_CODENAME "Mystic Spirit"
-#define STATUS_SERVER_PROTOCOL "9.81"
+#define STATUS_SERVER_PROTOCOL "9.82"
 #define STATUS_SERVER_VERSION "0.2.15"
 #define STATUS_SERVER_DEVELOPERS "Talaturen, Dalkon, Kornholijo and Elf"
